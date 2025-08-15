851bd1f45c2083ab3b4a27197d5da0162cbbea2a
Julian Hammerl <julian.hammerl@hotmail.com>
2025-08-15 09:27:36 -0600

    Add float converted rateControl adapter and algorithm

---
+++ algorithms/rateControl/rateControl.h
@@ -0,0 +1,42 @@
+/*
+ MIT License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#ifndef F32XIMERA_RATE_CONTROL_H
+#define F32XIMERA_RATE_CONTROL_H
+
+#include <stdint.h>
+#include <stdexcept>
+
+#include <Eigen/Dense>
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/messaging/messaging.h"
+#include "msgPayloadDef/AttGuidMsgF32Payload.h"
+#include "msgPayloadDef/CmdTorqueBodyMsgF32Payload.h"
+#include "msgPayloadDef/VehicleConfigMsgF32Payload.h"
+#include "rateControlAlgorithm.h"
+
+class RateControl : public SysModel {
+   public:
+    RateControl() = default;
+    ~RateControl() = default;
+
+    void reset(uint64_t currentSimNanos) override;
+    void updateState(uint64_t currentSimNanos) override;
+    void setDerivativeGainP(float P);
+    float getDerivativeGainP() const;
+    void setKnownTorquePntB_B(const Eigen::Vector3f &knownTorquePntB_B);
+    const Eigen::Vector3f &getKnownTorquePntB_B() const;
+
+    ReadFunctor<AttGuidMsgF32Payload> guidInMsg;             //!< Attitude guidance input message
+    ReadFunctor<VehicleConfigMsgF32Payload> vehConfigInMsg;  //!< Vehicle configuration input message
+    Message<CmdTorqueBodyMsgF32Payload> cmdTorqueOutMsg;     //!< Commanded torque output message
+
+   private:
+    RateControlAlgorithm algorithm{};
+};
+
+#endif
---
+++ algorithms/rateControl/rateControlAlgorithm.h
@@ -0,0 +1,35 @@
+/*
+ MIT License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+*/
+
+#ifndef F32XIMERA_RATE_CONTROL_ALGORITHM_H
+#define F32XIMERA_RATE_CONTROL_ALGORITHM_H
+
+#include <stdexcept>
+
+#include <Eigen/Dense>
+
+#include "msgPayloadDef/AttGuidMsgF32Payload.h"
+#include "msgPayloadDef/CmdTorqueBodyMsgF32Payload.h"
+#include "msgPayloadDef/VehicleConfigMsgF32Payload.h"
+
+class RateControlAlgorithm {
+   public:
+    CmdTorqueBodyMsgF32Payload update(AttGuidMsgF32Payload attGuidIn);
+    void setSpacecraftInertia(VehicleConfigMsgF32Payload vehicleConfigIn);
+    void setDerivativeGainP(float P);
+    float getDerivativeGainP() const;
+    void setKnownTorquePntB_B(const Eigen::Vector3f& knownTorquePntB_B);
+    const Eigen::Vector3f& getKnownTorquePntB_B() const;
+
+   private:
+    float P{};  //!< [N*m*s] Rate error feedback gain applied
+    Eigen::Vector3f knownTorquePntB_B{
+        Eigen::Vector3f::Zero()};  //!< [N*m] Known external torque expressed in body frame components
+    Eigen::Matrix3f ISCPntB_B{
+        Eigen::Matrix3f::Identity()};  //!< [kg*m^2] Spacecraft inertia about point B expressed in body frame components
+};
+
+#endif
