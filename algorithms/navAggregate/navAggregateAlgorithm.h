22cd95c594e94afce9b98e52af8f5da1860752b0
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-02 14:06:01 -0600

    Add float converted navAggregate adapter and algorithm

---
+++ algorithms/navAggregate/navAggregateAlgorithm.h
@@ -0,0 +1,65 @@
+/*
+ MIT License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#ifndef F32XIMERA_NAV_AGGREGATE_ALGORITHM_H
+#define F32XIMERA_NAV_AGGREGATE_ALGORITHM_H
+
+#include <stdint.h>
+#include <stdexcept>
+#include <string>
+
+#include <array>
+
+#include "msgPayloadDef/NavAttMsgF32Payload.h"
+#include "msgPayloadDef/NavTransMsgF32Payload.h"
+
+#define MAX_AGG_NAV_MSG 10
+
+/*! structure containing the attitude and translational navigation out messages */
+typedef struct {
+    NavAttMsgF32Payload navAttOut;     /*!< attitude navigation out message payload */
+    NavTransMsgF32Payload navTransOut; /*!< translation navigation out message payload */
+} AggregateOutput;
+
+class NavAggregateAlgorithm {
+   public:
+    AggregateOutput update(std::array<NavAttMsgF32Payload, MAX_AGG_NAV_MSG> attMsgsPayloads,
+                           std::array<NavTransMsgF32Payload, MAX_AGG_NAV_MSG> transMsgsPayloads);
+    void setAttTimeIdx(uint32_t idx);
+    uint32_t getAttTimeIdx() const;
+    void setTransTimeIdx(uint32_t idx);
+    uint32_t getTransTimeIdx() const;
+    void setAttIdx(uint32_t idx);
+    uint32_t getAttIdx() const;
+    void setRateIdx(uint32_t idx);
+    uint32_t getRateIdx() const;
+    void setPosIdx(uint32_t idx);
+    uint32_t getPosIdx() const;
+    void setVelIdx(uint32_t idx);
+    uint32_t getVelIdx() const;
+    void setDvIdx(uint32_t idx);
+    uint32_t getDvIdx() const;
+    void setSunIdx(uint32_t idx);
+    uint32_t getSunIdx() const;
+    void setAttMsgCount(uint32_t msgCount);
+    uint32_t getAttMsgCount() const;
+    void setTransMsgCount(uint32_t msgCount);
+    uint32_t getTransMsgCount() const;
+
+   private:
+    uint32_t attTimeIdx{};    /*!< [-] The index of the message to use for attitude message time */
+    uint32_t transTimeIdx{};  /*!< [-] The index of the message to use for translation message time */
+    uint32_t attIdx{};        /*!< [-] The index of the message to use for inertial MRP*/
+    uint32_t rateIdx{};       /*!< [-] The index of the message to use for attitude rate*/
+    uint32_t posIdx{};        /*!< [-] The index of the message to use for inertial position*/
+    uint32_t velIdx{};        /*!< [-] The index of the message to use for inertial velocity*/
+    uint32_t dvIdx{};         /*!< [-] The index of the message to use for accumulated DV */
+    uint32_t sunIdx{};        /*!< [-] The index of the message to use for sun pointing*/
+    uint32_t attMsgCount{};   /*!< [-] The total number of messages available as inputs */
+    uint32_t transMsgCount{}; /*!< [-] The total number of messages available as inputs */
+};
+
+#endif
---
+++ algorithms/navAggregate/navAggregateF32.i
@@ -0,0 +1,29 @@
+/*
+ MIT License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+*/
+
+%module navAggregateF32
+%{
+   #include "navAggregate.h"
+%}
+
+%pythoncode %{
+from Basilisk.architecture.swig_common_model import *
+%}
+
+%include "sys_model.i"
+%include "swig_conly_data.i"
+
+STRUCTASLIST(AggregateAttInput)
+STRUCTASLIST(AggregateTransInput)
+%include "navAggregate.h"
+%include "navAggregateAlgorithm.h"
+%include "msgPayloadDef/NavAttMsgF32Payload.h"
+%include "msgPayloadDef/NavTransMsgF32Payload.h"
+
+%pythoncode %{
+import sys
+protectAllClasses(sys.modules[__name__])
+%}
