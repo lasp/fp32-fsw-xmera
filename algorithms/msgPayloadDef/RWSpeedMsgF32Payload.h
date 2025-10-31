e8a6b4d57c0f9b542962612b07e214ae0304d75e
Julian Hammerl <julian.hammerl@hotmail.com>
2025-10-31 11:52:09 -0600

    Add float type message definitions for module

---
+++ algorithms/msgPayloadDef/RWSpeedMsgF32Payload.h
@@ -0,0 +1,18 @@
+/*
+ MIT License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#ifndef RW_SPEED_MESSAGE_F32_H
+#define RW_SPEED_MESSAGE_F32_H
+
+#include "architecture/msgPayloadDef/definitions.h"
+
+/*! @brief Structure used to define the output definition for reaction wheel speeds*/
+typedef struct {
+    float wheelSpeeds[RW_EFF_CNT];  //!< r/s The current angular velocities of the RW wheel
+    float wheelThetas[RW_EFF_CNT];  //!< rad The current angle of the RW if jitter is enabled
+} RWSpeedMsgF32Payload;
+
+#endif
