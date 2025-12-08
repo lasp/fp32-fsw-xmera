a1df917442c1a6a2a3dd87eba96072bbfee4d7f2
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2025-12-08 11:47:21 -0700

    drop

--- algorithms/rateControl/rateControlAlgorithm.cpp
+++ algorithms/rateControl/rateControlAlgorithm.cpp
@@ -1,7 +1,7 @@
 /*
  MIT License
 
- Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Bouldert
 */
 
 #include "rateControlAlgorithm.h"
