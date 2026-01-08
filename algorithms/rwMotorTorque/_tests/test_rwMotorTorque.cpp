190869c96e6a7922293a541af1e7067bba77c7af
Julian Hammerl <julian.hammerl@hotmail.com>
2026-01-08 15:53:30 -0700

    Add gtest

---
+++ algorithms/rwMotorTorque/_tests/test_rwMotorTorque.cpp
@@ -0,0 +1,13 @@
+#include "rwMotorTorqueTestHelpers.hpp"
+#include <gtest/gtest.h>
+
+TEST(RwMotorTorqueTest, ReferenceTest) { testRwMotorTorque(std::vector<float>{0.1, 0.2, 0.3},
+                                                           std::vector<float>{0.2, -0.4, 0.7},
+                                                           std::vector<bool>{false, false, true, false},
+                                                           true,
+                                                           true,
+                                                           4,
+                                                           std::vector<float>{0.4, 0.1, -0.3, 1.2, 0.4, 0.1, -0.3, 1.2, 0.4, 0.1, -0.3, 1.2},
+                                                           3); }
+
+TEST(RwMotorTorqueTest, SetupTest) { testRwMotorTorqueSetup(); }
