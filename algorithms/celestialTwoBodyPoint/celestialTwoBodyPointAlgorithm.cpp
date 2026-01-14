80c5c8d72df7bc514729aabfd6f66b65f9af291f
Cody Allard <cody.allard@lasp.colorado.edu>
2026-01-14 11:24:52 -0700

    Simplify notation by renaming variables

--- algorithms/celestialTwoBodyPoint/celestialTwoBodyPointAlgorithm.cpp
+++ algorithms/celestialTwoBodyPoint/celestialTwoBodyPointAlgorithm.cpp
@@ -17,43 +17,41 @@ void CelestialTwoBodyPointAlgorithm::reset(const bool secCelBodyIsLinked) {
 AttRefMsgF32Payload CelestialTwoBodyPointAlgorithm::update(EphemerisMsgF32Payload& celBodyIn,
                                                            EphemerisMsgF32Payload& secCelBodyIn,
                                                            NavTransMsgF32Payload& transNavIn) const {
-    const Eigen::Vector3d R_P1B_N =
+    const Eigen::Vector3d r_PB_N =
         Eigen::Map<const Eigen::Vector3d>(celBodyIn.r_BdyZero_N) - Eigen::Map<const Eigen::Vector3d>(transNavIn.r_BN_N);
-    const Eigen::Vector3d v_P1B_N =
+    const Eigen::Vector3d v_PB_N =
         Eigen::Map<const Eigen::Vector3d>(celBodyIn.v_BdyZero_N) - Eigen::Map<const Eigen::Vector3d>(transNavIn.v_BN_N);
 
-    Eigen::Vector3d R_P2B_N{};
-    Eigen::Vector3d v_P2B_N{};
+    Eigen::Vector3d r_SB_N{};
+    Eigen::Vector3d v_SB_N{};
 
     float platAngDiff{}; /* Angle between r_PB_N and r_SB_N */
     if (this->secCelBodyIsLinked) {
-        R_P2B_N = Eigen::Map<const Eigen::Vector3d>(secCelBodyIn.r_BdyZero_N) -
+        r_SB_N = Eigen::Map<const Eigen::Vector3d>(secCelBodyIn.r_BdyZero_N) -
                   Eigen::Map<const Eigen::Vector3d>(transNavIn.r_BN_N);
-        v_P2B_N = Eigen::Map<const Eigen::Vector3d>(secCelBodyIn.v_BdyZero_N) -
+        v_SB_N = Eigen::Map<const Eigen::Vector3d>(secCelBodyIn.v_BdyZero_N) -
                   Eigen::Map<const Eigen::Vector3d>(transNavIn.v_BN_N);
 
-        Eigen::Vector3f const R_P2B_N_hat = R_P2B_N.normalized().cast<float>();
-        Eigen::Vector3f const R_P1B_N_hat = R_P1B_N.normalized().cast<float>();
-        const float dotProduct = R_P2B_N_hat.dot(R_P1B_N_hat);
+        const float dotProduct = r_SB_N.normalized().dot(r_PB_N.normalized());
         platAngDiff = safeAcosf(dotProduct);
     }
 
-    /*! - Cross the P1 states to get R_P2, v_p2 and a_P2 */
-    if (!this->secCelBodyIsLinked || fabsf(platAngDiff) < this->singularityThresh ||
-        fabsf(platAngDiff) > M_PI - this->singularityThresh) {
-        R_P2B_N = R_P1B_N.cross(v_P1B_N);
-        v_P2B_N = Eigen::Vector3d::Zero();
+    /*! - Cross the first bodies' states to get R_SB and v_SB */
+    if (!this->secCelBodyIsLinked || fabs(platAngDiff) < this->singularityThresh ||
+        fabs(platAngDiff) > M_PI - this->singularityThresh) {
+        r_SB_N = r_PB_N.cross(v_PB_N);
+        v_SB_N = Eigen::Vector3d::Zero();
     }
 
     AttRefMsgF32Payload attRefOut{};
 
     /* - Initial computations: R_n, v_n, a_n */
-    const Eigen::Vector3d R_N = R_P1B_N.cross(R_P2B_N);
-    const Eigen::Vector3d v_N = v_P1B_N.cross(R_P2B_N) + R_P1B_N.cross(v_P2B_N);
-    const Eigen::Vector3d a_N = 2 * v_P1B_N.cross(v_P2B_N);
+    const Eigen::Vector3d R_N = r_PB_N.cross(r_SB_N);
+    const Eigen::Vector3d v_N = v_PB_N.cross(r_SB_N) + r_PB_N.cross(v_SB_N);
+    const Eigen::Vector3d a_N = 2 * v_PB_N.cross(v_SB_N);
 
     /* - Reference Frame computation */
-    const Eigen::Vector3f r1_N_hat = R_P1B_N.normalized().cast<float>();
+    const Eigen::Vector3f r1_N_hat = r_PB_N.normalized().cast<float>();
     const Eigen::Vector3f r3_N_hat = R_N.normalized().cast<float>();
     const Eigen::Vector3f r2_N_hat = (r3_N_hat.cross(r1_N_hat)).normalized().cast<float>();
     Eigen::Matrix3f dcm_RN{};
@@ -67,7 +65,7 @@ AttRefMsgF32Payload CelestialTwoBodyPointAlgorithm::update(EphemerisMsgF32Payloa
 
     /* - Reference base-vectors first time-derivative */
     const Eigen::Vector3f dr1_N_hat =
-        (Eigen::Matrix3f::Identity() - r1_N_hat * r1_N_hat.transpose()) * (v_P1B_N / R_P1B_N.norm()).cast<float>();
+        (Eigen::Matrix3f::Identity() - r1_N_hat * r1_N_hat.transpose()) * (v_PB_N / r_PB_N.norm()).cast<float>();
     const Eigen::Vector3f dr3_N_hat =
         (Eigen::Matrix3f::Identity() - r3_N_hat * r3_N_hat.transpose()) * (v_N / R_N.norm()).cast<float>();
     const Eigen::Vector3f dr2_N_hat = dr3_N_hat.cross(r1_N_hat) + r3_N_hat.cross(dr1_N_hat);
@@ -82,7 +80,7 @@ AttRefMsgF32Payload CelestialTwoBodyPointAlgorithm::update(EphemerisMsgF32Payloa
 
     /* - Reference base-vectors second time-derivative */
     const Eigen::Vector3f ddr1_N_hat = -(2 * dr1_N_hat * r1_N_hat.transpose() + r1_N_hat * dr1_N_hat.transpose()) *
-                                       (v_P1B_N / R_P1B_N.norm()).cast<float>();
+                                       (v_PB_N / r_PB_N.norm()).cast<float>();
     const Eigen::Vector3f ddr3_N_hat =
         ((Eigen::Matrix3f::Identity() - r3_N_hat * r3_N_hat.transpose()) * a_N.cast<float>() -
          (2 * dr3_N_hat * r3_N_hat.transpose() + r3_N_hat * dr3_N_hat.transpose()) * v_N.cast<float>()) /
