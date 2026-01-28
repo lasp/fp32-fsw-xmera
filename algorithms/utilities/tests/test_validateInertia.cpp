cf344d8090d2401c7b3bfaa39fce138f99852c50
tteil <thibaud.teil@lasp.colorado.edu>
2026-01-28 12:48:08 -0700

    add check function for inertia validity

---
+++ algorithms/utilities/tests/test_validateInertia.cpp
@@ -0,0 +1,13 @@
+#include "../validInertiaCheck.h"
+#include <gtest/gtest.h>
+
+TEST(ValidateInertiaTest, SetupTest) {
+    // --- Test expected exceptions ---
+    Eigen::Matrix3f badInertia = Eigen::Matrix3f::Identity();
+    badInertia << 1, 0, 0, 0, 1, 0, 0, 0, 0;
+    EXPECT_FALSE(inertiaIsValid(badInertia));
+    badInertia << 1, 0, 0, 0, 1, 0, 0, 1, 1;
+    EXPECT_FALSE(inertiaIsValid(badInertia));
+    badInertia << 3, 0, 0, 0, 1, 0, 0, 0, 1;
+    EXPECT_FALSE(inertiaIsValid(badInertia));
+}
---
+++ algorithms/utilities/tests/utilitiesHelpers.hpp
@@ -0,0 +1,56 @@
+#include "../validInertiaCheck.h"
+#include <gtest/gtest.h>
+#include <Eigen/Core>
+
+/* Function to generate a cross product operator from a vector */
+inline Eigen::Matrix3f tildeMatrix(const Eigen::Vector3f &vector) {
+    Eigen::Matrix3f tilde;
+    tilde << 0.0F, -vector(2), vector(1), vector(2), 0.0F, -vector(0), -vector(1), vector(0), 0.0F;
+    return tilde;
+}
+
+/* Function to generate a dcm from an mrp */
+inline Eigen::Matrix3f mrpToDcm(const Eigen::Vector3f &mrp) {
+    const Eigen::Matrix3f t = tildeMatrix(mrp);
+    const float denom = (1.0 + mrp.dot(mrp));
+    const Eigen::Matrix3f dcm = (8.0 * t * t - 4.0 * (1.0 - mrp.dot(mrp)) * t) / (denom * denom);
+
+    return dcm + Eigen::Matrix3f::Identity();
+}
+
+/* Function to generate a general inertia matrix */
+inline Eigen::Matrix3f GenerateValidInertiaMatrix(float const ev1,
+                                                  float const ev2,
+                                                  float const sigma1,
+                                                  float const sigma2,
+                                                  float const sigma3) {
+    /* Sort the first two eigenvalues so ev_min <= ev_max */
+    const float ev_min = std::min(ev1, ev2);
+    const float ev_max = std::max(ev1, ev2);
+    const float ev3_min = (ev_max - ev_min > 1e-5f) ? ev_max - ev_min + 1e-5f : 1e-5f;
+    const float ev3_max = ev_max + ev_min - 1e-5f;
+
+    const float ev3 = ev3_min + (0.5 * (ev3_max - ev3_min));
+    const std::array<float, 3> eigenvalues = {ev_min, ev3, ev_max};
+
+    /* Create a rotation matrix with provided mrp */
+    Eigen::Vector3f mrp(sigma1, sigma2, sigma3);
+    if (mrp.squaredNorm() > 1) {
+        mrp = -mrp / mrp.squaredNorm();
+    }
+    Eigen::Matrix3f R = mrpToDcm(mrp);
+
+    /* Create diagonal matrix D with eigenvalues */
+    Eigen::Matrix3f D;
+    D << eigenvalues[0], 0, 0, 0, eigenvalues[1], 0, 0, 0, eigenvalues[2];
+
+    /* Return rotated diagonal matrix */
+    return R.transpose() * D * R;
+}
+
+/* Test validity against generated inertia matrix */
+inline void testInertiaValidity(float eigen1, float eigen2, float sigma1, float sigma2, float sigma3) {
+    const Eigen::Matrix3f inertia = GenerateValidInertiaMatrix(eigen1, eigen2, sigma1, sigma2, sigma3);
+
+    EXPECT_TRUE(inertiaIsValid(inertia));
+}
