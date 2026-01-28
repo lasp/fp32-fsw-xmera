2283e5f5f865f5dd03282cd98115b065dd464161
Julian Hammerl <julian.hammerl@hotmail.com>
2026-01-28 11:58:11 -0700

    Use const where possible

--- algorithms/sunTrackError/sunTrackError.cpp
+++ algorithms/sunTrackError/sunTrackError.cpp
@@ -40,45 +40,45 @@ void SunTrackError::updateState(uint64_t callTime) {
 
     if (this->maneuverInitialized != 1) {
         if (this->transNavInMsg.isLinked() && this->ephemerisInMsg.isLinked()) {
-            Eigen::MRPd sigma_BN = cArrayToEigenMrp(nav.sigma_BN);
-            Eigen::MRPd sigma_R0N = cArrayToEigenMrp(ref.sigma_RN);
-            Eigen::MRPd sigmaLocal_R0R(this->sigma_R0R);
+            const Eigen::MRPd sigma_BN = cArrayToEigenMrp(nav.sigma_BN);
+            const Eigen::MRPd sigma_R0N = cArrayToEigenMrp(ref.sigma_RN);
+            const Eigen::MRPd sigmaLocal_R0R(this->sigma_R0R);
 
             NavTransMsgPayload navTrans = this->transNavInMsg();    //!< Get access to the spacecraft position
             EphemerisMsgPayload celState = this->ephemerisInMsg();  //!< Get access to the sun position
 
-            Eigen::Vector3d sHat_N = (cArrayToEigenVector(celState.r_BdyZero_N) - cArrayToEigenVector(navTrans.r_BN_N))
+            const Eigen::Vector3d sHat_N = (cArrayToEigenVector(celState.r_BdyZero_N) - cArrayToEigenVector(navTrans.r_BN_N))
                                          .normalized();  //!< inertial sun direction
 
-            Eigen::Matrix3d dcm_BN = sigma_BN.toRotationMatrix().transpose();
+            const Eigen::Matrix3d dcm_BN = sigma_BN.toRotationMatrix().transpose();
             // Define initial sensitive sun direction
-            Eigen::Vector3d senstiveInitial_N = dcm_BN.transpose() * this->sensitiveHat_B;
+            const Eigen::Vector3d senstiveInitial_N = dcm_BN.transpose() * this->sensitiveHat_B;
 
-            Eigen::Matrix3d dcm_R0N = sigma_R0N.toRotationMatrix().transpose();
-            Eigen::Matrix3d dcm_R0R = sigmaLocal_R0R.toRotationMatrix().transpose();
-            Eigen::Matrix3d dcm_BNFinal = (dcm_R0N.transpose() * dcm_R0R).transpose();
+            const Eigen::Matrix3d dcm_R0N = sigma_R0N.toRotationMatrix().transpose();
+            const Eigen::Matrix3d dcm_R0R = sigmaLocal_R0R.toRotationMatrix().transpose();
+            const Eigen::Matrix3d dcm_BNFinal = (dcm_R0N.transpose() * dcm_R0R).transpose();
             // Define final sensitive sun direction
-            Eigen::Vector3d senstiveFinal_N = dcm_BNFinal.transpose() * this->sensitiveHat_B;
+            const Eigen::Vector3d senstiveFinal_N = dcm_BNFinal.transpose() * this->sensitiveHat_B;
 
             // Define axis of rotation for sensitive sun direction
-            Eigen::Vector3d senstiveAxis_N = (senstiveInitial_N.cross(senstiveFinal_N)).normalized();
+            const Eigen::Vector3d senstiveAxis_N = (senstiveInitial_N.cross(senstiveFinal_N)).normalized();
             // Perform a Gram-Schmidt process to get a unit vector in the direction of sHat with no senstiveAxis_N comp
-            Eigen::Vector3d pHat_N = (sHat_N - (senstiveAxis_N.dot(sHat_N)) * senstiveAxis_N).normalized();
+            const Eigen::Vector3d pHat_N = (sHat_N - (senstiveAxis_N.dot(sHat_N)) * senstiveAxis_N).normalized();
 
             // Define total angle between initial and final directions of senstive surface
-            double initMnvrAngle = acos(senstiveInitial_N.dot(senstiveFinal_N));
+            const double initMnvrAngle = acos(senstiveInitial_N.dot(senstiveFinal_N));
             // Define the angle between the sHatDirection not in the rotation axis and initial sensitive direction
-            double initCelAngle = acos(pHat_N.dot(senstiveInitial_N));
+            const double initCelAngle = acos(pHat_N.dot(senstiveInitial_N));
 
-            Eigen::Matrix3d dcm_BR = dcm_BN * dcm_BNFinal.transpose();
-            Eigen::Vector3d prv_BR = dcmToPrv(dcm_BR);
+            const Eigen::Matrix3d dcm_BR = dcm_BN * dcm_BNFinal.transpose();
+            const Eigen::Vector3d prv_BR = dcmToPrv(dcm_BR);
             this->angleStart = prv_BR.norm();        //!< Find the principal rotation angle
             this->mnvrAxis_B = prv_BR.normalized();  //!< Find the principal rotation axis
 
-            Eigen::Vector3d sensToSunAxis_N = senstiveInitial_N.cross(sHat_N);  //!< This should be normalized, correct?
-            Eigen::Vector3d mnvrAxis_N = dcm_BN.transpose() * this->mnvrAxis_B;
+            const Eigen::Vector3d sensToSunAxis_N = senstiveInitial_N.cross(sHat_N);  //!< This should be normalized, correct?
+            const Eigen::Vector3d mnvrAxis_N = dcm_BN.transpose() * this->mnvrAxis_B;
             // Define dot product between the angle between how close the sun could move to the sensitive surface
-            double finalCelAngle = sensToSunAxis_N.dot(mnvrAxis_N);
+            const double finalCelAngle = sensToSunAxis_N.dot(mnvrAxis_N);
 
             // Logic to go the short or long rotation depending on sun avoidance
             if (finalCelAngle < 0.0 && initCelAngle < initMnvrAngle) {
@@ -104,22 +104,22 @@ AttGuidMsgPayload SunTrackError::computeSunTrackError(NavAttMsgPayload& nav,
                                                       AttRefMsgPayload& ref,
                                                       const uint64_t callTime) const {
     // Convert inputs to Eigen
-    Eigen::MRPd sigmaLocal_BN = cArrayToEigenMrp(nav.sigma_BN);
-    Eigen::Vector3d omegaLocal_BN_B = cArrayToEigenVector3(nav.omega_BN_B);
-    Eigen::MRPd sigmaLocal_R0N = cArrayToEigenMrp(ref.sigma_RN);
-    Eigen::Vector3d omegaLocal_RN_N = cArrayToEigenVector3(ref.omega_RN_N);
-    Eigen::Vector3d domegaLocal_RN_N = cArrayToEigenVector3(ref.domega_RN_N);
-    Eigen::MRPd sigmaLocal_R0R(this->sigma_R0R);
+    const Eigen::MRPd sigmaLocal_BN = cArrayToEigenMrp(nav.sigma_BN);
+    const Eigen::Vector3d omegaLocal_BN_B = cArrayToEigenVector3(nav.omega_BN_B);
+    const Eigen::MRPd sigmaLocal_R0N = cArrayToEigenMrp(ref.sigma_RN);
+    const Eigen::Vector3d omegaLocal_RN_N = cArrayToEigenVector3(ref.omega_RN_N);
+    const Eigen::Vector3d domegaLocal_RN_N = cArrayToEigenVector3(ref.domega_RN_N);
+    const Eigen::MRPd sigmaLocal_R0R(this->sigma_R0R);
 
     // Convert mrps to dcms
-    Eigen::Matrix3d dcm_BN = sigmaLocal_BN.toRotationMatrix().transpose();
-    Eigen::Matrix3d dcm_R0N = sigmaLocal_R0N.toRotationMatrix().transpose();
-    Eigen::Matrix3d dcm_R0R = sigmaLocal_R0R.toRotationMatrix().transpose();
+    const Eigen::Matrix3d dcm_BN = sigmaLocal_BN.toRotationMatrix().transpose();
+    const Eigen::Matrix3d dcm_R0N = sigmaLocal_R0N.toRotationMatrix().transpose();
+    const Eigen::Matrix3d dcm_R0R = sigmaLocal_R0R.toRotationMatrix().transpose();
 
     // This calculation can be seen in attitude tracking documentation
-    Eigen::Matrix3d dcm_RN = (dcm_R0N.transpose() * dcm_R0R).transpose();
+    const Eigen::Matrix3d dcm_RN = (dcm_R0N.transpose() * dcm_R0R).transpose();
 
-    double dtSeconds = nsToSec(callTime - this->mnvrStartTime);
+    const double dtSeconds = nsToSec(callTime - this->mnvrStartTime);
 
     // Integrate the angle to provide a feed forward rate
     double relativeAngleCurr = this->angleStart - this->angleRate * dtSeconds;
@@ -129,17 +129,17 @@ AttGuidMsgPayload SunTrackError::computeSunTrackError(NavAttMsgPayload& nav,
     AttGuidMsgPayload attGuidOut{};
 
     // This calculation can be seen in attitude tracking documentation
-    Eigen::Vector3d prv_BR = relativeAngleCurr * this->mnvrAxis_B;
-    Eigen::Matrix3d dcmCmd_BR = prvToDcm(prv_BR);
-    Eigen::Matrix3d dcm_BR = dcm_BN * (dcmCmd_BR * dcm_RN).transpose();
-    Eigen::Vector3d sigmaLocal_BR = dcmToMrp(dcm_BR);
+    const Eigen::Vector3d prv_BR = relativeAngleCurr * this->mnvrAxis_B;
+    const Eigen::Matrix3d dcmCmd_BR = prvToDcm(prv_BR);
+    const Eigen::Matrix3d dcm_BR = dcm_BN * (dcmCmd_BR * dcm_RN).transpose();
+    const Eigen::Vector3d sigmaLocal_BR = dcmToMrp(dcm_BR);
     eigenVectorToCArray(sigmaLocal_BR, attGuidOut.sigma_BR);
 
     // This calculation can be seen in attitude tracking documentation
     Eigen::Vector3d omegaLocal_RN_B = dcm_BN * omegaLocal_RN_N;
     eigenVectorToCArray(omegaLocal_RN_B, attGuidOut.omega_RN_B);
 
-    Eigen::Vector3d omegaCatchup_BN_B = -this->angleRate * this->mnvrAxis_B;
+    const Eigen::Vector3d omegaCatchup_BN_B = -this->angleRate * this->mnvrAxis_B;
     // Logic to provide the feedforward rate
     if (relativeAngleCurr > 0.0) {
         omegaLocal_RN_B = omegaLocal_RN_B + omegaCatchup_BN_B;
@@ -147,10 +147,10 @@ AttGuidMsgPayload SunTrackError::computeSunTrackError(NavAttMsgPayload& nav,
     }
 
     // Perform remaining attitude tracking calculations
-    Eigen::Vector3d omegaLocal_BR_B = omegaLocal_BN_B - omegaLocal_RN_B;
+    const Eigen::Vector3d omegaLocal_BR_B = omegaLocal_BN_B - omegaLocal_RN_B;
     eigenVectorToCArray(omegaLocal_BR_B, attGuidOut.omega_BR_B);
 
-    Eigen::Vector3d domegaLocal_RN_B = dcm_BN * domegaLocal_RN_N;
+    const Eigen::Vector3d domegaLocal_RN_B = dcm_BN * domegaLocal_RN_N;
     eigenVectorToCArray(domegaLocal_RN_B, attGuidOut.domega_RN_B);  //!< compute reference d(omega)/dt in body frame components
 
     return attGuidOut;
