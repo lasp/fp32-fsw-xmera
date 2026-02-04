885c9040743870eb2b8f96b0afefe76620130c23
Julian Hammerl <julian.hammerl@hotmail.com>
2026-02-03 17:19:15 -0700

    Add gtest

---
+++ algorithms/navAggregate/_tests/test_navAggregate.cpp
@@ -0,0 +1,42 @@
+#include "navAggregateTestHelpers.hpp"
+#include <gtest/gtest.h>
+
+TEST(NavAggregateTest, ReferenceTest) {
+    testNavAggregate(7489.3,
+                     std::vector<float>{234.5, -243.23, 134.13},
+                     std::vector<float>{-867.89, 342.34, -876.67},
+                     std::vector<float>{4.89, 6.12, 423.48},
+                     100.1,
+                     std::vector<double>{10060.5, -239847.23, 82148.13},
+                     std::vector<double>{-3786.89, 234798.34, -7389.67},
+                     std::vector<float>{2345.89, 3432.12, 2342.48},
+                     1,
+                     2,
+                     3,
+                     4,
+                     5,
+                     6,
+                     7,
+                     8,
+                     10,
+                     10);
+}
+
+TEST(NavAggregateTest, SetupTest) {
+    NavAggregateAlgorithm alg{};
+
+    // --- Test expected exceptions ---
+
+    // MsgIdx equal to or greater than max message count
+    EXPECT_THROW(alg.setAttTimeIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setTransTimeIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setAttIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setRateIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setPosIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setVelIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setDvIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    EXPECT_THROW(alg.setSunIdx(MAX_AGG_NAV_MSG), fs::invalid_argument);
+    // MsgCount greater than max message count
+    EXPECT_THROW(alg.setAttMsgCount(MAX_AGG_NAV_MSG + 1), fs::invalid_argument);
+    EXPECT_THROW(alg.setTransMsgCount(MAX_AGG_NAV_MSG + 1), fs::invalid_argument);
+}
