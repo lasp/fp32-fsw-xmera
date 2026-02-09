1647d33e9bf8e34a336ff5ffb812c6b113402bd3
Julian Hammerl <julian.hammerl@hotmail.com>
2026-02-09 16:54:10 -0700

    Remove license text

--- algorithms/mrpSteering/mrpSteeringAlgorithm.cpp
+++ algorithms/mrpSteering/mrpSteeringAlgorithm.cpp
@@ -1,9 +1,3 @@
-/*
- MIT License
-
- Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
- */
-
 #include "mrpSteeringAlgorithm.h"
 #include "../freestandingInvalidArgument.h"
 #include "../utilities/validInertiaCheck.h"
--- algorithms/mrpSteering/mrpSteeringF32.i
+++ algorithms/mrpSteering/mrpSteeringF32.i
@@ -1,9 +1,3 @@
-/*
- MIT License
-
- Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
- */
-
 %module mrpSteeringF32
 %{
    #include "mrpSteering.h"
