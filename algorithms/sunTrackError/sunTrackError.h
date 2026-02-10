905d36a1770abd9f278d8c912538efbfd8f74c00
Julian Hammerl <julian.hammerl@hotmail.com>
2026-02-10 13:33:30 -0700

    Initialize class members

--- algorithms/sunTrackError/sunTrackError.h
+++ algorithms/sunTrackError/sunTrackError.h
@@ -35,15 +35,15 @@ class SunTrackError : public SysModel {
     void updateState(uint64_t callTime) override;
 
    private:
-    Eigen::Vector3d sigma_R0R;       /*!< MRP from corrected reference frame to original frame R0
-                                    This is the same as [BcB] going from primary body frame B
-                                    to the corrected body frame Bc */
-    Eigen::Vector3d sensitiveHat_B;  //!< [-] Vehicle body vector to exclude from sun*/
-    double angleRate;                //!< [r/s] The rate at which we maneuver to Sun point*/
-    Eigen::Vector3d mnvrAxis_B;  //!< [-] Eigen axis that we are maneuvering on*/
-    double angleStart;           //!< [r] The angle remaining in the attitude maneuver*/
-    bool maneuverInitialized;     //!< [-] Flag indicating if maneuver has been set*/
-    uint64_t mnvrStartTime;      //!< [ns] Time at which the maneuver was begun*/
+    Eigen::Vector3d sigma_R0R{Eigen::Vector3d::Zero()};  /*!< MRP from corrected reference frame to original frame R0
+                                                            This is the same as [BcB] going from primary body frame B
+                                                            to the corrected body frame Bc */
+    Eigen::Vector3d sensitiveHat_B{Eigen::Vector3d::Zero()};  //!< [-] Vehicle body vector to exclude from sun*/
+    double angleRate{};                //!< [r/s] The rate at which we maneuver to Sun point*/
+    Eigen::Vector3d mnvrAxis_B{Eigen::Vector3d::Zero()};  //!< [-] Eigen axis that we are maneuvering on*/
+    double angleStart{};           //!< [r] The angle remaining in the attitude maneuver*/
+    bool maneuverInitialized{};     //!< [-] Flag indicating if maneuver has been set*/
+    uint64_t mnvrStartTime{};      //!< [ns] Time at which the maneuver was begun*/
 
     AttGuidMsgPayload computeSunTrackError(NavAttMsgPayload& nav,
                                            AttRefMsgPayload& ref,
