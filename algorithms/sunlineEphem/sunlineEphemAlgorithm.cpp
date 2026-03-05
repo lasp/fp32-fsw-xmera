d7685f7bac95bb860817d224e46c03e80793f4ea
Cody Allard <cody.allard@lasp.colorado.edu>
2026-03-05 13:32:29 -0700

    Simplifying logic in sunlineEphem algorithm

--- algorithms/sunlineEphem/sunlineEphemAlgorithm.cpp
+++ algorithms/sunlineEphem/sunlineEphemAlgorithm.cpp
@@ -7,24 +7,14 @@
 Eigen::Vector3f SunlineEphemAlgorithm::update(const Eigen::Vector3d& r_SN_N,
                                               const Eigen::Vector3d& r_BN_N,
                                               const Eigen::Vector3f& sigma_BN) const {
-    // Subtract the two position vectors to find the position of the sun as seen from the body
+    // Compute sun-to-body relative position in inertial frame
     const Eigen::Vector3d r_SB_N = r_SN_N - r_BN_N;
-    // Normalize this vector to find the sun direction as seen from the body
-    Eigen::Vector3f rHat_SB_N = Eigen::Vector3f::Zero();
+
+    // Normalize, rotate into body frame, and re-normalize (or zero if colocated)
+    Eigen::Vector3f rHat_SB_B = Eigen::Vector3f::Zero();
     if (r_SB_N.norm() > std::numeric_limits<double>::epsilon()) {
-        rHat_SB_N = r_SB_N.normalized().cast<float>();
-    }
-    // Build direction cosine matrix from spacecraft attitude
-    const Eigen::Matrix3f dcm_BN = mrpToDcm(sigma_BN);
-
-    // Rotate into body frame
-    Eigen::Vector3f rHat_SB_B = dcm_BN * rHat_SB_N;
-
-    // Ensure unit length (or zero)
-    if (rHat_SB_B.norm() > std::numeric_limits<float>::epsilon()) {
-        rHat_SB_B.normalize();  // in-place unit-length
-    } else {
-        rHat_SB_B.setZero();  // explicit zero
+        const Eigen::Matrix3f dcm_BN = mrpToDcm(sigma_BN);
+        rHat_SB_B = (dcm_BN * r_SB_N.normalized().cast<float>()).normalized();
     }
 
     return rHat_SB_B;
