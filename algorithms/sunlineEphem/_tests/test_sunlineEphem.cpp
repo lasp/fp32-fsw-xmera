a6fc379c3874c3ad010022d0c3d96e256c7a1756
Cody Allard <cody.allard@lasp.colorado.edu>
2026-03-05 13:33:57 -0700

    Add property test for colocation

--- algorithms/sunlineEphem/_tests/test_sunlineEphem.cpp
+++ algorithms/sunlineEphem/_tests/test_sunlineEphem.cpp
@@ -7,3 +7,19 @@ TEST(SunlineEphemTest, ReferenceTest) {
                      std::vector<float>{0.1f, 0.2f, -0.3f}       // non-trivial attitude
     );
 }
+
+TEST(SunlineEphemTest, Colocation) {
+    // When sun and spacecraft are at the same position, the output should be a zero vector
+    SunlineEphemAlgorithm alg{};
+
+    Eigen::Vector3d r_SN_N(1.0, 2.0, 3.0);
+    Eigen::Vector3d r_BN_N(1.0, 2.0, 3.0);
+    Eigen::Vector3f sigma_BN(0.1f, 0.2f, -0.3f);
+
+    Eigen::Vector3f out;
+    EXPECT_NO_THROW(out = alg.update(r_SN_N, r_BN_N, sigma_BN));
+
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_NEAR(out[i], 0.0f, 1e-6);
+    }
+}
