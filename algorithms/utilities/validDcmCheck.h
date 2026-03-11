87fcfafb4389b08e4a48dcfeb2266392a559e2c5
tteil <thibaud.teil@lasp.colorado.edu>
2026-03-11 14:52:02 -0600

    remove cmath from validDcmCheck

--- algorithms/utilities/validDcmCheck.h
+++ algorithms/utilities/validDcmCheck.h
@@ -2,13 +2,14 @@
 #define FP32_XMERA_FSW_ALGORITHMS_VALID_DCM_H
 
 #include <Eigen/Core>
+#include <math.h>
 
 inline constexpr float kToleranceF = 1.0e-5F;
 
 inline bool isValidDcm(Eigen::Matrix3f const& dcm) {
     bool valid = true;
     const bool isOrthogonal = (dcm.transpose() * dcm).isApprox(Eigen::Matrix3f::Identity(), kToleranceF);
-    const bool detIsValid = std::abs(dcm.determinant() - 1.0F) <= kToleranceF;
+    const bool detIsValid = fabsf(dcm.determinant() - 1.0F) <= kToleranceF;
     if (!isOrthogonal || !detIsValid) {
         valid = false;
     }
