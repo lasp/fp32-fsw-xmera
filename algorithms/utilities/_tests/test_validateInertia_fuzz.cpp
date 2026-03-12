31ccb52a9f8a06bf59482bc1a56019c698149e4a
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-03-12 11:13:54 -0600

    Colocation valid inertia test with fuzz macro

--- algorithms/utilities/_tests/test_validateInertia_fuzz.cpp
+++ algorithms/utilities/_tests/test_validateInertia_fuzz.cpp
@@ -1,9 +1,20 @@
 #include "utilitiesHelpers.hpp"
 #include <fuzztest/fuzztest.h>
 
+/* Test validity against generated inertia matrix */
+inline void testInertiaValidity(const double eigen1,
+                                const double eigen2,
+                                const double sigma1,
+                                const double sigma2,
+                                const double sigma3) {
+    const Eigen::Matrix3f inertia = generateValidInertiaMatrix(eigen1, eigen2, sigma1, sigma2, sigma3).cast<float>();
+
+    EXPECT_TRUE(inertiaIsValid(inertia));
+}
+
 FUZZ_TEST(InertiaFuzz, testInertiaValidity)
-    .WithDomains(fuzztest::InRange(1.0F, 1e6F),
-                 fuzztest::InRange(1.0F, 1e6F),
-                 fuzztest::InRange(1e-6F, 1.0F),
-                 fuzztest::InRange(1e-6F, 1.0F),
-                 fuzztest::InRange(1e-6F, 1.0F));
+    .WithDomains(fuzztest::InRange(1.0, 1e6),
+                 fuzztest::InRange(1.0, 1e6),
+                 fuzztest::InRange(1e-6, 1.0),
+                 fuzztest::InRange(1e-6, 1.0),
+                 fuzztest::InRange(1e-6, 1.0));
