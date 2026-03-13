9d1af79a607311290a4d466b0bd617cdd4e61c18
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-03-13 11:46:44 -0600

    Refactor generation of inertia matrix to double

--- algorithms/utilities/_tests/utilitiesHelpers.hpp
+++ algorithms/utilities/_tests/utilitiesHelpers.hpp
@@ -4,49 +4,48 @@
 #include <gtest/gtest.h>
 #include <Eigen/Core>
 
-/* Function to generate a cross product operator from a vector */
-inline Eigen::Matrix3f tildeMatrix(const Eigen::Vector3f& vector) {
-    Eigen::Matrix3f tilde;
-    tilde << 0.0F, -vector(2), vector(1), vector(2), 0.0F, -vector(0), -vector(1), vector(0), 0.0F;
+/* Function to generate a cross product operator from a vector (double precision) */
+inline Eigen::Matrix3d tildeMatrix(const Eigen::Vector3d& vector) {
+    Eigen::Matrix3d tilde;
+    tilde << 0.0, -vector(2), vector(1), vector(2), 0.0, -vector(0), -vector(1), vector(0), 0.0;
     return tilde;
 }
 
-/* Function to generate a dcm from an mrp */
-inline Eigen::Matrix3f mrpToDcm(const Eigen::Vector3f& mrp) {
-    const Eigen::Matrix3f t = tildeMatrix(mrp);
-    const float denom = (1.0 + mrp.dot(mrp));
-    const Eigen::Matrix3f dcm = (8.0 * t * t - 4.0 * (1.0 - mrp.dot(mrp)) * t) / (denom * denom);
+/* Function to generate a dcm from an mrp (double precision) */
+inline Eigen::Matrix3d mrpToDcm(const Eigen::Vector3d& mrp) {
+    const Eigen::Matrix3d t = tildeMatrix(mrp);
+    const double denom = (1.0 + mrp.dot(mrp));
+    const Eigen::Matrix3d dcm = (8.0 * t * t - 4.0 * (1.0 - mrp.dot(mrp)) * t) / (denom * denom);
 
-    return dcm + Eigen::Matrix3f::Identity();
+    return dcm + Eigen::Matrix3d::Identity();
 }
 
-/* Function to generate a general inertia matrix */
-inline Eigen::Matrix3f GenerateValidInertiaMatrix(float const ev1,
-                                                  float const ev2,
-                                                  float const sigma1,
-                                                  float const sigma2,
-                                                  float const sigma3) {
+/* Function to generate a general inertia matrix using double precision internally */
+inline Eigen::Matrix3d generateValidInertiaMatrix(double const ev1,
+                                                  double const ev2,
+                                                  double const sigma1,
+                                                  double const sigma2,
+                                                  double const sigma3) {
     /* Sort the first two eigenvalues so ev_min <= ev_max */
-    const float ev_min = std::min(ev1, ev2);
-    const float ev_max = std::max(ev1, ev2);
-    const float ev3_min = (ev_max - ev_min > 1e-5f) ? ev_max - ev_min + 1e-5f : 1e-5f;
-    const float ev3_max = ev_max + ev_min - 1e-5f;
+    const double ev_min = std::min(ev1, ev2);
+    const double ev_max = std::max(ev1, ev2);
+    const double ev3_min = (ev_max - ev_min > 1e-5) ? ev_max - ev_min + 1e-5 : 1e-5;
+    const double ev3_max = ev_max + ev_min - 1e-5;
 
-    const float ev3 = ev3_min + (0.5 * (ev3_max - ev3_min));
-    const std::array<float, 3> eigenvalues = {ev_min, ev3, ev_max};
+    const double ev3 = ev3_min + (0.5 * (ev3_max - ev3_min));
 
-    /* Create a rotation matrix with provided mrp */
-    Eigen::Vector3f mrp(sigma1, sigma2, sigma3);
+    /* Create a rotation matrix with provided mrp (double precision) */
+    Eigen::Vector3d mrp(static_cast<double>(sigma1), static_cast<double>(sigma2), static_cast<double>(sigma3));
     if (mrp.squaredNorm() > 1) {
         mrp = -mrp / mrp.squaredNorm();
     }
-    Eigen::Matrix3f R = mrpToDcm(mrp);
+    Eigen::Matrix3d R = mrpToDcm(mrp);
 
     /* Create diagonal matrix D with eigenvalues */
-    Eigen::Matrix3f D;
-    D << eigenvalues[0], 0, 0, 0, eigenvalues[1], 0, 0, 0, eigenvalues[2];
+    Eigen::Matrix3d D;
+    D << ev_min, 0, 0, 0, ev3, 0, 0, 0, ev_max;
 
-    /* Return rotated diagonal matrix */
+    /* Return rotated diagonal matrix, cast to float */
     return R.transpose() * D * R;
 }
 
