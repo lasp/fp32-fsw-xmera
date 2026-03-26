5e8f5bb3b4b6b9acaa7edcc9e6e4eb2a11c2b868
Julian Hammerl <julian.hammerl@hotmail.com>
2026-03-26 11:39:18 -0600

    Delete ephemNavConverter

--- algorithms/ephemNavConverter/_tests/test_ephemNavConverter_fuzz.cpp
+++
@@ -1,7 +0,0 @@
-#include "ephemNavConverterTestHelpers.hpp"
-#include <fuzztest/fuzztest.h>
-
-FUZZ_TEST(EphemNavConverterAlgorithmFuzz, testEphemNavConverter)
-    .WithDomains(fuzztest::Finite<double>(),
-                 fuzztest::VectorOf(fuzztest::Finite<double>()).WithSize(3),
-                 fuzztest::VectorOf(fuzztest::Finite<double>()).WithSize(3));
