c92213003cbd884dba8f009f5db662f7d2d9ca65
Julian Hammerl <julian.hammerl@hotmail.com>
2026-03-26 13:39:23 -0600

    Use one regression test function for single and fuzzed test

--- algorithms/bodyRateMiscompare/_tests/test_bodyRateMiscompare.cpp
+++ algorithms/bodyRateMiscompare/_tests/test_bodyRateMiscompare.cpp
@@ -3,49 +3,6 @@
 #include <gtest/gtest.h>
 #include <array>
 
-static void runRegressionCase(float threshold,
-                              uint32_t faultPersistenceLimit,
-                              const std::array<float, 3>& imuVec,
-                              const std::array<float, 3>& stVec,
-                              bool useImuRates = false) {
-    BodyRateMiscompareAlgorithm alg{};
-    alg.setBodyRateThreshold(threshold);
-    alg.setFaultPersistenceLimit(faultPersistenceLimit);
-    alg.setUseImuRates(useImuRates);
-
-    const Eigen::Vector3f imu = toEigenVector(imuVec);
-    const Eigen::Vector3f st = toEigenVector(stVec);
-
-    bool refFaultDetected = useImuRates;
-    uint32_t refFaultPersistenceCount = 0;
-
-    int numSteps = 5;
-    for (int step = 0; step < numSteps; ++step) {
-        BodyRateMiscompareOutput out{};
-        BodyRateMiscompareOutput ref{};
-
-        EXPECT_NO_THROW(out = alg.update(imu, st));
-        EXPECT_NO_THROW(ref = referenceUpdate(
-                            threshold, faultPersistenceLimit, refFaultDetected, refFaultPersistenceCount, imu, st));
-
-        for (int i = 0; i < 3; ++i) {
-            EXPECT_FLOAT_EQ(out.omega_BN_B[i], ref.omega_BN_B[i]);
-            EXPECT_TRUE(std::isfinite(out.omega_BN_B[i]));
-        }
-        EXPECT_EQ(out.bodyRateFaultDetected, ref.bodyRateFaultDetected);
-
-        if (out.bodyRateFaultDetected) {
-            for (int i = 0; i < 3; ++i) {
-                EXPECT_FLOAT_EQ(out.omega_BN_B[i], imu[i]);
-            }
-        } else {
-            for (int i = 0; i < 3; ++i) {
-                EXPECT_FLOAT_EQ(out.omega_BN_B[i], st[i]);
-            }
-        }
-    }
-}
-
 TEST(BodyRateMiscompareTest, RegressionTestNoFault) {
     runRegressionCase(1.0F, 1, {0.1F, -0.2F, 0.3F}, {0.2F, -0.1F, 0.1F});
 }
--- algorithms/bodyRateMiscompare/_tests/test_bodyRateMiscompare_fuzz.cpp
+++ algorithms/bodyRateMiscompare/_tests/test_bodyRateMiscompare_fuzz.cpp
@@ -1,43 +1,12 @@
 #include "bodyRateMiscompareTestHelpers.hpp"
 #include <fuzztest/fuzztest.h>
 
-inline void fuzzPropertyBodyRateMiscompare(float threshold,
-                                           uint32_t faultPersistenceLimit,
-                                           const std::array<float, 3>& imuVec,
-                                           const std::array<float, 3>& stVec) {
-    BodyRateMiscompareAlgorithm alg{};
-    alg.setBodyRateThreshold(threshold);
-    alg.setFaultPersistenceLimit(faultPersistenceLimit);
-
-    const Eigen::Vector3f imu = toEigenVector(imuVec);
-    const Eigen::Vector3f st = toEigenVector(stVec);
-
-    bool refFaultDetected = false;
-    uint32_t refFaultPersistenceCount = 0;
-
-    int numSteps = 5;
-    for (int step = 0; step < numSteps; ++step) {
-        BodyRateMiscompareOutput out{};
-        EXPECT_NO_THROW(out = alg.update(imu, st));
-
-        BodyRateMiscompareOutput ref{};
-        EXPECT_NO_THROW(ref = referenceUpdate(
-                            threshold, faultPersistenceLimit, refFaultDetected, refFaultPersistenceCount, imu, st));
-
-        EXPECT_EQ(out.bodyRateFaultDetected, ref.bodyRateFaultDetected);
-
-        for (int i = 0; i < 3; ++i) {
-            EXPECT_FLOAT_EQ(out.omega_BN_B[i], ref.omega_BN_B[i]);
-            EXPECT_TRUE(std::isfinite(out.omega_BN_B[i]));
-        }
-    }
-}
-
-FUZZ_TEST(BodyRateMiscompareAlgorithmFuzz, fuzzPropertyBodyRateMiscompare)
+FUZZ_TEST(BodyRateMiscompareAlgorithmFuzz, runRegressionCase)
     .WithDomains(fuzztest::InRange(1e-6F, 1e6F),
                  fuzztest::InRange(1U, 10U),
                  fuzztest::ArrayOf<3>(fuzztest::InRange(-1e6F, 1e6F)),
-                 fuzztest::ArrayOf<3>(fuzztest::InRange(-1e6F, 1e6F)));
+                 fuzztest::ArrayOf<3>(fuzztest::InRange(-1e6F, 1e6F)),
+                 fuzztest::Arbitrary<bool>());
 
 // ---------------------------------------------------------------------------
 // Property fuzz tests
