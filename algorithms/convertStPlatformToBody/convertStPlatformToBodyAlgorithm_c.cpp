fc92d4db645531f658a8cff8d2022f474d92375f
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-07 19:09:38 -0600

    Add C shim for ConvertStPlatformToBodyAlgorithm
    Provides an extern "C" interface with opaque handle for use by
    non-C++ consumers. Uses Matrix3f_c POD type for the mounting DCM
    parameter.

---
+++ algorithms/convertStPlatformToBody/convertStPlatformToBodyAlgorithm_c.cpp
@@ -0,0 +1,36 @@
+/*
+ MIT License
+
+ Copyright (c) 2026, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#include "convertStPlatformToBodyAlgorithm_c.h"
+#include "architecture/utilities/eigenSupport.h"
+#include "convertStPlatformToBodyAlgorithm.h"
+
+#include <Eigen/Core>
+
+ConvertStPlatformToBodyAlgorithm* ConvertStPlatformToBodyAlgorithm_create(void) {
+    return reinterpret_cast<ConvertStPlatformToBodyAlgorithm*>(new ::ConvertStPlatformToBodyAlgorithm());
+}
+
+void ConvertStPlatformToBodyAlgorithm_destroy(ConvertStPlatformToBodyAlgorithm* self) {
+    delete reinterpret_cast<::ConvertStPlatformToBodyAlgorithm*>(self);
+}
+
+StAttitudeOutput ConvertStPlatformToBodyAlgorithm_update(ConvertStPlatformToBodyAlgorithm* self,
+                                                         StSensorInput* stSensorIn) {
+    return reinterpret_cast<::ConvertStPlatformToBodyAlgorithm*>(self)->update(*stSensorIn);
+}
+
+void ConvertStPlatformToBodyAlgorithm_setDcmCB(ConvertStPlatformToBodyAlgorithm* self, Matrix3f_c dcm_CB) {
+    Eigen::Matrix3f mat = c2DArrayToEigenMatrix3(dcm_CB.data);
+    reinterpret_cast<::ConvertStPlatformToBodyAlgorithm*>(self)->setDcmCB(mat);
+}
+
+Matrix3f_c ConvertStPlatformToBodyAlgorithm_getDcmCB(const ConvertStPlatformToBodyAlgorithm* self) {
+    Eigen::Matrix3f mat = reinterpret_cast<const ::ConvertStPlatformToBodyAlgorithm*>(self)->getDcmCB();
+    Matrix3f_c out;
+    eigenMatrixToCArray2D(mat, out.data);
+    return out;
+}
