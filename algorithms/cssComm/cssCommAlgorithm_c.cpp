d556873cd13225c9812bc4ffd59855fb161cf101
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-13 15:37:46 -0600

    Add cssComm algorithm C shim

---
+++ algorithms/cssComm/cssCommAlgorithm_c.cpp
@@ -0,0 +1,68 @@
+/* MIT License
+ *
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#include "cssCommAlgorithm_c.h"
+#include "cssCommAlgorithm.h"
+
+#include <algorithm>
+
+CssCommAlgorithm* CssCommAlgorithm_create(void) {
+    return reinterpret_cast<CssCommAlgorithm*>(new ::CssCommAlgorithm());
+}
+
+void CssCommAlgorithm_destroy(CssCommAlgorithm* self) { delete reinterpret_cast<::CssCommAlgorithm*>(self); }
+
+CssSensorValues_c CssCommAlgorithm_update(const CssCommAlgorithm* self, const CssSensorValues_c* inputValues) {
+    std::array<double, kMaxNumCssSensors> input{};
+    std::copy(inputValues->data, inputValues->data + kMaxNumCssSensors, input.begin());
+
+    std::array<double, kMaxNumCssSensors> result = reinterpret_cast<const ::CssCommAlgorithm*>(self)->update(input);
+
+    CssSensorValues_c out{};
+    std::copy(result.begin(), result.end(), out.data);
+    return out;
+}
+
+void CssCommAlgorithm_setNumSensors(CssCommAlgorithm* self, const uint32_t numberOfSensors) {
+    reinterpret_cast<::CssCommAlgorithm*>(self)->setNumSensors(numberOfSensors);
+}
+
+uint32_t CssCommAlgorithm_getNumSensors(const CssCommAlgorithm* self) {
+    return reinterpret_cast<const ::CssCommAlgorithm*>(self)->getNumSensors();
+}
+
+void CssCommAlgorithm_setMaxSensorValue(CssCommAlgorithm* self, const double maxValue) {
+    reinterpret_cast<::CssCommAlgorithm*>(self)->setMaxSensorValue(maxValue);
+}
+
+double CssCommAlgorithm_getMaxSensorValue(const CssCommAlgorithm* self) {
+    return reinterpret_cast<const ::CssCommAlgorithm*>(self)->getMaxSensorValue();
+}
+
+void CssCommAlgorithm_setChebyCount(CssCommAlgorithm* self, const uint32_t count) {
+    reinterpret_cast<::CssCommAlgorithm*>(self)->setChebyCount(count);
+}
+
+uint32_t CssCommAlgorithm_getChebyCount(const CssCommAlgorithm* self) {
+    return reinterpret_cast<const ::CssCommAlgorithm*>(self)->getChebyCount();
+}
+
+void CssCommAlgorithm_setChebyPolynomials(CssCommAlgorithm* self, const ChebyPolynomials_c* polynomials) {
+    std::array<double, kMaxNumChebyPolys> arr{};
+    std::copy(polynomials->data, polynomials->data + kMaxNumChebyPolys, arr.begin());
+    reinterpret_cast<::CssCommAlgorithm*>(self)->setChebyPolynomials(arr);
+}
+
+ChebyPolynomials_c CssCommAlgorithm_getChebyPolynomials(const CssCommAlgorithm* self) {
+    std::array<double, kMaxNumChebyPolys> arr =
+        reinterpret_cast<const ::CssCommAlgorithm*>(self)->getChebyPolynomials();
+    ChebyPolynomials_c out{};
+    std::copy(arr.begin(), arr.end(), out.data);
+    return out;
+}
+
+uint32_t CssCommAlgorithm_getMaxNumCssSensors(void) { return kMaxNumCssSensors; }
+
+uint32_t CssCommAlgorithm_getMaxNumChebyPolys(void) { return kMaxNumChebyPolys; }
---
+++ algorithms/cssComm/cssCommAlgorithm_c.h
@@ -0,0 +1,128 @@
+/* MIT License
+ *
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#ifndef F32XMERA_CSS_COMM_ALGORITHM_C_H
+#define F32XMERA_CSS_COMM_ALGORITHM_C_H
+
+#include "cssCommTypes.h"
+#include "msgPayloadDef/definitions.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Opaque handle to the C++ CssCommAlgorithm instance.
+ */
+typedef struct CssCommAlgorithm CssCommAlgorithm;
+
+/**
+ * @brief Bounded array of CSS sensor values (double precision).
+ */
+typedef struct {
+    double data[MAX_NUM_CSS_SENSORS];
+} CssSensorValues_c;
+
+/**
+ * @brief Bounded array of Chebyshev polynomial coefficients.
+ */
+typedef struct {
+    double data[MAX_NUM_CHEBY_POLYS];
+} ChebyPolynomials_c;
+
+/**
+ * @brief Construct a new CssCommAlgorithm instance.
+ * @return Pointer to a new CssCommAlgorithm (must be destroyed).
+ */
+CssCommAlgorithm* CssCommAlgorithm_create(void);
+
+/**
+ * @brief Destroy a previously created CssCommAlgorithm.
+ * @param self Pointer to the instance to destroy.
+ */
+void CssCommAlgorithm_destroy(CssCommAlgorithm* self);
+
+/**
+ * @brief Run the CSS communication correction update.
+ * @param self        Pointer to the instance.
+ * @param inputValues Pointer to the input CSS sensor values.
+ * @return CssSensorValues_c The corrected CSS sensor values.
+ */
+CssSensorValues_c CssCommAlgorithm_update(const CssCommAlgorithm* self, const CssSensorValues_c* inputValues);
+
+/**
+ * @brief Set the number of CSS sensors.
+ * @param self            Pointer to the instance.
+ * @param numberOfSensors The number of CSS sensors to process.
+ */
+void CssCommAlgorithm_setNumSensors(CssCommAlgorithm* self, uint32_t numberOfSensors);
+
+/**
+ * @brief Get the number of CSS sensors.
+ * @param self Pointer to the instance.
+ * @return uint32_t The number of CSS sensors.
+ */
+uint32_t CssCommAlgorithm_getNumSensors(const CssCommAlgorithm* self);
+
+/**
+ * @brief Set the maximum sensor value (scale factor).
+ * @param self     Pointer to the instance.
+ * @param maxValue The maximum sensor value.
+ */
+void CssCommAlgorithm_setMaxSensorValue(CssCommAlgorithm* self, double maxValue);
+
+/**
+ * @brief Get the maximum sensor value (scale factor).
+ * @param self Pointer to the instance.
+ * @return double The maximum sensor value.
+ */
+double CssCommAlgorithm_getMaxSensorValue(const CssCommAlgorithm* self);
+
+/**
+ * @brief Set the Chebyshev polynomial count.
+ * @param self  Pointer to the instance.
+ * @param count The number of Chebyshev polynomials.
+ */
+void CssCommAlgorithm_setChebyCount(CssCommAlgorithm* self, uint32_t count);
+
+/**
+ * @brief Get the Chebyshev polynomial count.
+ * @param self Pointer to the instance.
+ * @return uint32_t The number of Chebyshev polynomials.
+ */
+uint32_t CssCommAlgorithm_getChebyCount(const CssCommAlgorithm* self);
+
+/**
+ * @brief Set the Chebyshev polynomial coefficients.
+ * @param self        Pointer to the instance.
+ * @param polynomials Pointer to the polynomial coefficients.
+ */
+void CssCommAlgorithm_setChebyPolynomials(CssCommAlgorithm* self, const ChebyPolynomials_c* polynomials);
+
+/**
+ * @brief Get the Chebyshev polynomial coefficients.
+ * @param self Pointer to the instance.
+ * @return ChebyPolynomials_c The polynomial coefficients.
+ */
+ChebyPolynomials_c CssCommAlgorithm_getChebyPolynomials(const CssCommAlgorithm* self);
+
+/**
+ * @brief Get the MAX_NUM_CSS_SENSORS constant for Ada validation.
+ * @return The value of MAX_NUM_CSS_SENSORS.
+ */
+uint32_t CssCommAlgorithm_getMaxNumCssSensors(void);
+
+/**
+ * @brief Get the MAX_NUM_CHEBY_POLYS constant for Ada validation.
+ * @return The value of MAX_NUM_CHEBY_POLYS.
+ */
+uint32_t CssCommAlgorithm_getMaxNumChebyPolys(void);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // F32XMERA_CSS_COMM_ALGORITHM_C_H
