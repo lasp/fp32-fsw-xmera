6f92992f33a8cff73aae664e3229c52f596905a3
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-15 13:49:08 -0600

    Convert to FP32

--- algorithms/triad/triad.cpp
+++ algorithms/triad/triad.cpp
@@ -10,7 +10,7 @@
 #include <architecture/utilities/eigenSupport.h>
 #include <architecture/utilities/rigidBodyKinematics.hpp>
 
-static constexpr double kNormEpsilon = 1e-12;
+static constexpr float kNormEpsilon = 1e-6F;
 
 void Triad::reset(const uint64_t callTime) {
     if (!this->attNavInMsg.isLinked()) {
@@ -47,52 +47,53 @@ void Triad::reset(const uint64_t callTime) {
 }
 
 void Triad::updateState(const uint64_t callTime) {
-    AttRefMsgPayload attRefOut = {};
-    const NavAttMsgPayload attNavIn = this->attNavInMsg();
+    AttRefMsgF32Payload attRefOut = {};
+    const NavAttMsgF32Payload attNavIn = this->attNavInMsg();
 
     // Resolve inertial heading
-    Eigen::Vector3d hReqHat_N;
+    Eigen::Vector3f hReqHat_N;
     if (this->inertialAxisInput == InertialAxisInput::inputInertialHeadingParameter) {
         hReqHat_N = this->hHat_N.normalized();
     } else if (this->inertialAxisInput == InertialAxisInput::inputInertialHeadingMsg) {
-        const InertialHeadingMsgPayload inertialHeadingIn = this->inertialHeadingInMsg();
+        const InertialHeadingMsgF32Payload inertialHeadingIn = this->inertialHeadingInMsg();
         hReqHat_N = cArrayToEigenVector(inertialHeadingIn.rHat_XN_N).normalized();
     } else if (this->inertialAxisInput == InertialAxisInput::inputEphemerisMsg) {
-        const EphemerisMsgPayload ephemerisIn = this->ephemerisInMsg();
-        const NavTransMsgPayload transNavIn = this->transNavInMsg();
-        hReqHat_N =
-            (cArrayToEigenVector(ephemerisIn.r_BdyZero_N) - cArrayToEigenVector(transNavIn.r_BN_N)).normalized();
+        const EphemerisMsgF32Payload ephemerisIn = this->ephemerisInMsg();
+        const NavTransMsgF32Payload transNavIn = this->transNavInMsg();
+        hReqHat_N = (cArrayToEigenVector(ephemerisIn.r_BdyZero_N) - cArrayToEigenVector(transNavIn.r_BN_N))
+                        .normalized()
+                        .cast<float>();
     }
 
     // Resolve body heading
-    Eigen::Vector3d hRefHat_B;
+    Eigen::Vector3f hRefHat_B;
     if (this->bodyAxisInput == BodyAxisInput::inputBodyHeadingParameter) {
         hRefHat_B = this->h1Hat_B.normalized();
     } else if (this->bodyAxisInput == BodyAxisInput::inputBodyHeadingMsg) {
-        const BodyHeadingMsgPayload bodyHeadingIn = this->bodyHeadingInMsg();
+        const BodyHeadingMsgF32Payload bodyHeadingIn = this->bodyHeadingInMsg();
         hRefHat_B = cArrayToEigenVector(bodyHeadingIn.rHat_XB_B).normalized();
     }
 
     // Compute sun direction in inertial frame
-    const Eigen::Matrix3d BN = mrpToDcm(cArrayToEigenVector(attNavIn.sigma_BN));
-    const Eigen::Vector3d rHat_SB_B = cArrayToEigenVector(attNavIn.vehSunPntBdy).normalized();
-    const Eigen::Vector3d rHat_SB_N = BN.transpose() * rHat_SB_B;
+    const Eigen::Matrix3f BN = mrpToDcm(cArrayToEigenVector(attNavIn.sigma_BN));
+    const Eigen::Vector3f rHat_SB_B = cArrayToEigenVector(attNavIn.vehSunPntBdy).normalized();
+    const Eigen::Vector3f rHat_SB_N = BN.transpose() * rHat_SB_B;
 
     // Run algorithm
-    const Eigen::Vector3d sigma_RN = this->algorithm.update(rHat_SB_N, hReqHat_N, hRefHat_B);
+    const Eigen::Vector3f sigma_RN = this->algorithm.update(rHat_SB_N, hReqHat_N, hRefHat_B);
 
     eigenVectorToCArray(sigma_RN, attRefOut.sigma_RN);
     this->attRefOutMsg.write(&attRefOut, this->moduleID, callTime);
 }
 
-void Triad::setA1Hat_B(const Eigen::Vector3d& a1Hat_B) { this->algorithm.setA1Hat_B(a1Hat_B); }
-Eigen::Vector3d Triad::getA1Hat_B() const { return this->algorithm.getA1Hat_B(); }
+void Triad::setA1Hat_B(const Eigen::Vector3f& a1Hat_B) { this->algorithm.setA1Hat_B(a1Hat_B); }
+Eigen::Vector3f Triad::getA1Hat_B() const { return this->algorithm.getA1Hat_B(); }
 
-void Triad::setH1Hat_B(const Eigen::Vector3d& h1Hat_B) { this->h1Hat_B = h1Hat_B; }
-Eigen::Vector3d Triad::getH1Hat_B() const { return this->h1Hat_B; }
+void Triad::setH1Hat_B(const Eigen::Vector3f& h1Hat_B) { this->h1Hat_B = h1Hat_B; }
+Eigen::Vector3f Triad::getH1Hat_B() const { return this->h1Hat_B; }
 
-void Triad::setHHat_N(const Eigen::Vector3d& hHat_N) { this->hHat_N = hHat_N; }
-Eigen::Vector3d Triad::getHHat_N() const { return this->hHat_N; }
+void Triad::setHHat_N(const Eigen::Vector3f& hHat_N) { this->hHat_N = hHat_N; }
+Eigen::Vector3f Triad::getHHat_N() const { return this->hHat_N; }
 
 void Triad::setCelestialBodyInput(const CelestialBody& celestialBodyInput) {
     this->celestialBodyInput = celestialBodyInput;
--- algorithms/triad/triadAlgorithm.h
+++ algorithms/triad/triadAlgorithm.h
@@ -7,15 +7,15 @@
 
 class TriadAlgorithm final {
    public:
-    Eigen::Vector3d update(const Eigen::Vector3d& rHat_SB_N,
-                           const Eigen::Vector3d& hReqHat_N,
-                           const Eigen::Vector3d& hRefHat_B) const;
+    Eigen::Vector3f update(const Eigen::Vector3f& rHat_SB_N,
+                           const Eigen::Vector3f& hReqHat_N,
+                           const Eigen::Vector3f& hRefHat_B) const;
 
-    void setA1Hat_B(const Eigen::Vector3d& a1Hat_B);
-    Eigen::Vector3d getA1Hat_B() const;
+    void setA1Hat_B(const Eigen::Vector3f& a1Hat_B);
+    Eigen::Vector3f getA1Hat_B() const;
 
    private:
-    Eigen::Vector3d a1Hat_B = Eigen::Vector3d::Zero();
+    Eigen::Vector3f a1Hat_B = Eigen::Vector3f::Zero();
 };
 
 #endif
