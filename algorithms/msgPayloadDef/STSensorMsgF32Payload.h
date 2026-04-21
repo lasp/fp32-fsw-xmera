dd39f9b10cd2853937cece957dece63704fc7e43
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-21 11:21:29 -0600

    Add STSensorMsgF32Payload message type

---
+++ algorithms/msgPayloadDef/STSensorMsgF32Payload.h
@@ -0,0 +1,17 @@
+/*
+ MIT License
+
+ Copyright (c) 2026, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+ */
+
+#ifndef ST_SENSOR_MESSAGE_F32_H
+#define ST_SENSOR_MESSAGE_F32_H
+
+/*! @brief Star tracker sensor message (F32) */
+typedef struct {
+    float timeTag;         //!< [s] Time tag placed on the output state
+    float qInrtl2Case[4];  //!< [-] Quaternion to go from the inertial to case
+    float omega_CN_C[3];   //!< [rad/s] Inertial angular velocity in case frame
+} STSensorMsgF32Payload;
+
+#endif
