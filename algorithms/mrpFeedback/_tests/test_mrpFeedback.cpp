b6fa5acd68eb47c58ac54fdcea90c3142f82e2fd
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-24 14:14:45 -0600

    Migrate mrpFeedback tests to Eigen fuzz domain helpers

--- algorithms/mrpFeedback/_tests/test_mrpFeedback.cpp
+++ algorithms/mrpFeedback/_tests/test_mrpFeedback.cpp
@@ -2,16 +2,16 @@
 #include <gtest/gtest.h>
 
 TEST(MrpFeedbackTest, ReferenceTest) {
-    testMrpFeedback(std::vector<float>{0.4, 0.1, -0.3},
+    testMrpFeedback(Eigen::Vector3f{0.4, 0.1, -0.3},
                     1.0,
                     0.4,
                     0.1,
                     1.1,
                     0,
-                    std::vector<float>{0.1, -0.2, 0.3},
-                    std::vector<float>{-0.4, 0.5, -0.6},
-                    std::vector<float>{0.7, -0.8, 0.9},
-                    std::vector<float>{-1.0, 1.1, -1.2},
+                    Eigen::Vector3f{0.1, -0.2, 0.3},
+                    Eigen::Vector3f{-0.4, 0.5, -0.6},
+                    Eigen::Vector3f{0.7, -0.8, 0.9},
+                    Eigen::Vector3f{-1.0, 1.1, -1.2},
                     std::vector<float>{1.9, -2.0, 2.1, -2.2},
                     std::vector<bool>{false, true, false, false},
                     2,
--- algorithms/mrpFeedback/_tests/test_mrpFeedback_fuzz.cpp
+++ algorithms/mrpFeedback/_tests/test_mrpFeedback_fuzz.cpp
@@ -1,17 +1,18 @@
+#include "architecture/testUtilities/eigenFuzzDomains.hpp"
 #include "mrpFeedbackTestHelpers.hpp"
 #include <fuzztest/fuzztest.h>
 
 FUZZ_TEST(MrpFeedbackAlgorithmFuzz, testMrpFeedback)
-    .WithDomains(fuzztest::VectorOf(fuzztest::InRange(-2e0F, 2e0F)).WithSize(3),                // sigma_BR_B
+    .WithDomains(xmera::fuzz::Vector3fInRange(-2e0F, 2e0F),                                     // sigma_BR_B
                  fuzztest::InRange(0.0F, 1e3F),                                                 // K
                  fuzztest::InRange(0.0F, 1e3F),                                                 // P
                  fuzztest::InRange(0.0F, 1e3F),                                                 // Ki
                  fuzztest::InRange(0.0F, 1e3F),                                                 // integralLimit
                  fuzztest::InRange(0, 1),                                                       // controlLawType
-                 fuzztest::VectorOf(fuzztest::InRange(-1e1F, 1e1F)).WithSize(3U),               // knownTorquePntB_B
-                 fuzztest::VectorOf(fuzztest::InRange(-1e0F, 1e0F)).WithSize(3U),               // omega_BR_B
-                 fuzztest::VectorOf(fuzztest::InRange(-1e0F, 1e0F)).WithSize(3U),               // omega_RN_B
-                 fuzztest::VectorOf(fuzztest::InRange(-1e0F, 1e0F)).WithSize(3U),               // domega_RN_B
+                 xmera::fuzz::Vector3fInRange(-1e1F, 1e1F),                                     // knownTorquePntB_B
+                 xmera::fuzz::Vector3fInRange(-1e0F, 1e0F),                                     // omega_BR_B
+                 xmera::fuzz::Vector3fInRange(-1e0F, 1e0F),                                     // omega_RN_B
+                 xmera::fuzz::Vector3fInRange(-1e0F, 1e0F),                                     // domega_RN_B
                  fuzztest::VectorOf(fuzztest::InRange(-1e3F, 1e3F)).WithSize(RW_EFF_CNT),       // wheelSpeeds
                  fuzztest::VectorOf(fuzztest::Arbitrary<bool>()).WithSize(RW_EFF_CNT),          // wheelAvailabilityBool
                  fuzztest::InRange(0, RW_EFF_CNT),                                              // numRW
