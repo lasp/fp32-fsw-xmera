cde9377d7f71d567a89640f28445f3170f6389df
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-01 16:56:32 -0600

    Add C shim for dvGuidance

---
+++ algorithms/dvGuidance/dvGuidanceAlgorithm_c.cpp
@@ -0,0 +1,37 @@
+#include "dvGuidanceAlgorithm_c.h"
+#include "architecture/utilities/eigenSupport.h"
+#include "dvGuidanceAlgorithm.h"
+
+#include <Eigen/Core>
+
+DvGuidanceAlgorithm* DvGuidanceAlgorithm_create(void) {
+    // clang-format off
+    return reinterpret_cast<DvGuidanceAlgorithm*>(new ::DvGuidanceAlgorithm(DvGuidanceConfig::create()));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
+    // clang-format on
+}
+
+void DvGuidanceAlgorithm_destroy(DvGuidanceAlgorithm* self) {
+    // clang-format off
+    delete reinterpret_cast<::DvGuidanceAlgorithm*>(self);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
+    // clang-format on
+}
+
+AttRefMsgF32Payload DvGuidanceAlgorithm_update(const DvGuidanceAlgorithm* self,
+                                               Vector3f_c dvInrtlCmd,
+                                               Vector3f_c dvRotVecUnit,
+                                               float dvRotVecMag,
+                                               uint64_t burnStartTime,
+                                               uint64_t callTime) {
+    const Eigen::Vector3f dvInrtlCmd_e = cArrayToEigenVector3<float>(dvInrtlCmd.data);
+    const Eigen::Vector3f dvRotVecUnit_e = cArrayToEigenVector3<float>(dvRotVecUnit.data);
+
+    // clang-format off
+    const DvGuidanceOutput out = reinterpret_cast<const ::DvGuidanceAlgorithm*>(self)->update(dvInrtlCmd_e, dvRotVecUnit_e, dvRotVecMag, burnStartTime, callTime);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    // clang-format on
+
+    AttRefMsgF32Payload payload{};
+    eigenVectorToCArray(out.sigma_RN, payload.sigma_RN);
+    eigenVectorToCArray(out.omega_RN_N, payload.omega_RN_N);
+    eigenVectorToCArray(out.domega_RN_N, payload.domega_RN_N);
+    return payload;
+}
