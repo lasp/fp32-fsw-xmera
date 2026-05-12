34d8badc274e340045b9e3fa71d358834077fef1
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-11 19:49:26 -0600

    Change max num css sensors back to 32

--- algorithms/msgPayloadDef/definitions.h
+++ algorithms/msgPayloadDef/definitions.h
@@ -3,10 +3,10 @@
 
 #include <cstdint>
 
-#define MAX_NUM_CSS_SENSORS 16
+#define MAX_NUM_CSS_SENSORS 32
 inline constexpr std::uint32_t kMaxNumCssSensors = MAX_NUM_CSS_SENSORS;
 #define MAX_EFF_CNT 36
-inline constexpr std::uint32_t kMaxThrusterCount = 36U;
+inline constexpr std::uint32_t kMaxThrusterCount = MAX_EFF_CNT;
 inline constexpr std::uint32_t kMimuCount = 3U;
 #define RW_EFF_CNT 36
 
