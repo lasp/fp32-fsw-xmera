cc774787d83482bdd3a02ab98175f9324d34f74b
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-26 12:32:16 -0600

    Restructure bodyRateMiscompareAlg Types.h to pure C

--- algorithms/bodyRateMiscompare/bodyRateMiscompareTypes.h
+++ algorithms/bodyRateMiscompare/bodyRateMiscompareTypes.h
@@ -1,20 +1,4 @@
 #ifndef F32XMERA_BODY_RATE_MISCOMPARE_OUTPUT_H
 #define F32XMERA_BODY_RATE_MISCOMPARE_OUTPUT_H
 
-#include <Eigen/Core>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*! @brief Structure containing the body frame angular rate and body rate fault output */
-typedef struct {
-    Eigen::Vector3f omega_BN_B; /*!< body frame angular rate */
-    bool bodyRateFaultDetected; /*!< body rate fault */
-} BodyRateMiscompareOutput;
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
-
 #endif  // F32XMERA_BODY_RATE_MISCOMPARE_OUTPUT_H
