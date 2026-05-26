09a19aeea80621c37b73e69cee296f6c73fb0792
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-26 11:51:33 -0600

    Refactor mrpRotationAlg C shim alg handle type name

--- algorithms/mrpRotation/mrpRotationAlgorithm_c.cpp
+++ algorithms/mrpRotation/mrpRotationAlgorithm_c.cpp
@@ -13,31 +13,31 @@ MrpRotationConfig configFromC(const MrpRotationConfig_c& c) {
 }
 }  // namespace
 
-MrpRotationAlgorithm* MrpRotationAlgorithm_create(const MrpRotationConfig_c* config) {
+MrpRotationAlgorithmHandle* MrpRotationAlgorithm_create(const MrpRotationConfig_c* config) {
     // clang-format off
-    return reinterpret_cast<MrpRotationAlgorithm*>(new ::MrpRotationAlgorithm(configFromC(*config)));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
+    return reinterpret_cast<MrpRotationAlgorithmHandle*>(new ::MrpRotationAlgorithm(configFromC(*config)));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
     // clang-format on
 }
 
-void MrpRotationAlgorithm_destroy(MrpRotationAlgorithm* self) {
+void MrpRotationAlgorithm_destroy(MrpRotationAlgorithmHandle* self) {
     // clang-format off
     delete reinterpret_cast<::MrpRotationAlgorithm*>(self);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-owning-memory)
     // clang-format on
 }
 
-void MrpRotationAlgorithm_setConfig(MrpRotationAlgorithm* self, const MrpRotationConfig_c* config) {
+void MrpRotationAlgorithm_setConfig(MrpRotationAlgorithmHandle* self, const MrpRotationConfig_c* config) {
     // clang-format off
     reinterpret_cast<::MrpRotationAlgorithm*>(self)->setConfig(configFromC(*config));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
     // clang-format on
 }
 
-void MrpRotationAlgorithm_reset(MrpRotationAlgorithm* self) {
+void MrpRotationAlgorithm_reset(MrpRotationAlgorithmHandle* self) {
     // clang-format off
     reinterpret_cast<::MrpRotationAlgorithm*>(self)->reset();  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
     // clang-format on
 }
 
-AttRefMsgF32Payload MrpRotationAlgorithm_update(MrpRotationAlgorithm* self,
+AttRefMsgF32Payload MrpRotationAlgorithm_update(MrpRotationAlgorithmHandle* self,
                                                 uint64_t callTime,
                                                 const AttRefMsgF32Payload* inputRef,
                                                 const AttStateMsgF32Payload* attStates) {
