e89a4dcfd1111361a61750c1203a716a11b6bbc7
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-26 11:51:33 -0600

    Refactor inertial3DAlg C shim alg handle type name

--- algorithms/inertial3D/inertial3DAlgorithm_c.h
+++ algorithms/inertial3D/inertial3DAlgorithm_c.h
@@ -11,40 +11,40 @@ extern "C" {
 /**
  * @brief Opaque handle to the C++ Inertial3DAlgorithm instance.
  */
-typedef struct Inertial3DAlgorithm Inertial3DAlgorithm;
+typedef struct Inertial3DAlgorithmHandle Inertial3DAlgorithmHandle;
 
 /**
  * @brief Construct a new Inertial3DAlgorithm instance.
  * @return Pointer to a new Inertial3DAlgorithm (must be destroyed).
  */
-Inertial3DAlgorithm* Inertial3DAlgorithm_create(void);
+Inertial3DAlgorithmHandle* Inertial3DAlgorithm_create(void);
 
 /**
  * @brief Destroy a previously created Inertial3DAlgorithm.
  * @param self Pointer to the instance to destroy.
  */
-void Inertial3DAlgorithm_destroy(Inertial3DAlgorithm* self);
+void Inertial3DAlgorithm_destroy(Inertial3DAlgorithmHandle* self);
 
 /**
  * @brief Run the update step.
  * @param self Pointer to the instance.
  * @return AttRefMsgF32Payload  The computed reference message.
  */
-AttRefMsgF32Payload Inertial3DAlgorithm_update(const Inertial3DAlgorithm* self);
+AttRefMsgF32Payload Inertial3DAlgorithm_update(const Inertial3DAlgorithmHandle* self);
 
 /**
  * @brief Set the σ_RN three-vector.
  * @param self    Pointer to the instance.
  * @param sigmaRN 3-vector in flattened POD format.
  */
-void Inertial3DAlgorithm_setSigmaRN(Inertial3DAlgorithm* self, Vector3f_c sigmaRN);
+void Inertial3DAlgorithm_setSigmaRN(Inertial3DAlgorithmHandle* self, Vector3f_c sigmaRN);
 
 /**
  * @brief Get the current σ_RN three-vector.
  * @param self Pointer to the instance.
  * @return Vector3f_c  Flattened POD containing the vector.
  */
-Vector3f_c Inertial3DAlgorithm_getSigmaRN(const Inertial3DAlgorithm* self);
+Vector3f_c Inertial3DAlgorithm_getSigmaRN(const Inertial3DAlgorithmHandle* self);
 
 #ifdef __cplusplus
 }  // extern "C"
