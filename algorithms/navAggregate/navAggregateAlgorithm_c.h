d3f1692337c77c32cc98ad08e9a70a4895795d80
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-26 11:51:33 -0600

    Refactor navAggregateAlg C shim alg handle type name

--- algorithms/navAggregate/navAggregateAlgorithm_c.h
+++ algorithms/navAggregate/navAggregateAlgorithm_c.h
@@ -12,7 +12,7 @@ extern "C" {
 /**
  * @brief Opaque handle to the C++ NavAggregateAlgorithm instance.
  */
-typedef struct NavAggregateAlgorithm NavAggregateAlgorithm;
+typedef struct NavAggregateAlgorithmHandle NavAggregateAlgorithmHandle;
 
 /**
  * @brief Maximum number of aggregate navigation messages.
@@ -52,13 +52,13 @@ uint32_t NavAggregateAlgorithm_getMaxAggNavMsg(void);
  * @brief Construct a new NavAggregateAlgorithm instance.
  * @return Pointer to a new NavAggregateAlgorithm (must be destroyed).
  */
-NavAggregateAlgorithm* NavAggregateAlgorithm_create(void);
+NavAggregateAlgorithmHandle* NavAggregateAlgorithm_create(void);
 
 /**
  * @brief Destroy a previously created NavAggregateAlgorithm.
  * @param self Pointer to the instance to destroy.
  */
-void NavAggregateAlgorithm_destroy(NavAggregateAlgorithm* self);
+void NavAggregateAlgorithm_destroy(NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Run the update step.
@@ -67,7 +67,7 @@ void NavAggregateAlgorithm_destroy(NavAggregateAlgorithm* self);
  * @param transMsgsPayloads  Translational navigation message payloads.
  * @return AggregateOutput_c The computed output messages.
  */
-AggregateOutput_c NavAggregateAlgorithm_update(NavAggregateAlgorithm* self,
+AggregateOutput_c NavAggregateAlgorithm_update(NavAggregateAlgorithmHandle* self,
                                                const NavAttMsgF32PayloadArray10_c* attMsgsPayloads,
                                                const NavTransMsgF32PayloadArray10_c* transMsgsPayloads);
 
@@ -76,140 +76,140 @@ AggregateOutput_c NavAggregateAlgorithm_update(NavAggregateAlgorithm* self,
  * @param self Pointer to the instance.
  * @param idx  The new attitude time index to set.
  */
-void NavAggregateAlgorithm_setAttTimeIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setAttTimeIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current attitude time index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current attitude time index.
  */
-uint32_t NavAggregateAlgorithm_getAttTimeIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getAttTimeIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the translation time index.
  * @param self Pointer to the instance.
  * @param idx  The new translation time index to set.
  */
-void NavAggregateAlgorithm_setTransTimeIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setTransTimeIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current translation time index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current translation time index.
  */
-uint32_t NavAggregateAlgorithm_getTransTimeIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getTransTimeIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the attitude index.
  * @param self Pointer to the instance.
  * @param idx  The new attitude index to set.
  */
-void NavAggregateAlgorithm_setAttIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setAttIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current attitude index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current attitude index.
  */
-uint32_t NavAggregateAlgorithm_getAttIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getAttIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the rate index.
  * @param self Pointer to the instance.
  * @param idx  The new rate index to set.
  */
-void NavAggregateAlgorithm_setRateIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setRateIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current rate index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current rate index.
  */
-uint32_t NavAggregateAlgorithm_getRateIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getRateIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the position index.
  * @param self Pointer to the instance.
  * @param idx  The new position index to set.
  */
-void NavAggregateAlgorithm_setPosIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setPosIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current position index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current position index.
  */
-uint32_t NavAggregateAlgorithm_getPosIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getPosIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the velocity index.
  * @param self Pointer to the instance.
  * @param idx  The new velocity index to set.
  */
-void NavAggregateAlgorithm_setVelIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setVelIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current velocity index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current velocity index.
  */
-uint32_t NavAggregateAlgorithm_getVelIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getVelIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the accumulated DV index.
  * @param self Pointer to the instance.
  * @param idx  The new accumulated DV index to set.
  */
-void NavAggregateAlgorithm_setDvIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setDvIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current accumulated DV index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current accumulated DV index.
  */
-uint32_t NavAggregateAlgorithm_getDvIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getDvIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the sun index.
  * @param self Pointer to the instance.
  * @param idx  The new sun index to set.
  */
-void NavAggregateAlgorithm_setSunIdx(NavAggregateAlgorithm* self, uint32_t idx);
+void NavAggregateAlgorithm_setSunIdx(NavAggregateAlgorithmHandle* self, uint32_t idx);
 
 /**
  * @brief Get the current sun index.
  * @param self Pointer to the instance.
  * @return uint32_t  The current sun index.
  */
-uint32_t NavAggregateAlgorithm_getSunIdx(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getSunIdx(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the attitude message count.
  * @param self     Pointer to the instance.
  * @param msgCount The new attitude message count to set.
  */
-void NavAggregateAlgorithm_setAttMsgCount(NavAggregateAlgorithm* self, uint32_t msgCount);
+void NavAggregateAlgorithm_setAttMsgCount(NavAggregateAlgorithmHandle* self, uint32_t msgCount);
 
 /**
  * @brief Get the current attitude message count.
  * @param self Pointer to the instance.
  * @return uint32_t  The current attitude message count.
  */
-uint32_t NavAggregateAlgorithm_getAttMsgCount(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getAttMsgCount(const NavAggregateAlgorithmHandle* self);
 
 /**
  * @brief Set the translational message count.
  * @param self     Pointer to the instance.
  * @param msgCount The new translational message count to set.
  */
-void NavAggregateAlgorithm_setTransMsgCount(NavAggregateAlgorithm* self, uint32_t msgCount);
+void NavAggregateAlgorithm_setTransMsgCount(NavAggregateAlgorithmHandle* self, uint32_t msgCount);
 
 /**
  * @brief Get the current translational message count.
  * @param self Pointer to the instance.
  * @return uint32_t  The current translational message count.
  */
-uint32_t NavAggregateAlgorithm_getTransMsgCount(const NavAggregateAlgorithm* self);
+uint32_t NavAggregateAlgorithm_getTransMsgCount(const NavAggregateAlgorithmHandle* self);
 
 #ifdef __cplusplus
 }  // extern "C"
