d0b039a63ef5b276df344e9ddd0d9df50781c070
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-05-26 12:30:58 -0600

    Restructure dvGuidanceAlg Types.h to pure C

--- algorithms/dvGuidance/dvGuidanceTypes.h
+++ algorithms/dvGuidance/dvGuidanceTypes.h
@@ -1,12 +1,4 @@
 #ifndef F32XMERA_DV_GUIDANCE_TYPES_H
 #define F32XMERA_DV_GUIDANCE_TYPES_H
 
-#include <Eigen/Core>
-
-struct DvGuidanceOutput {
-    Eigen::Vector3f sigma_RN = Eigen::Vector3f::Zero();
-    Eigen::Vector3f omega_RN_N = Eigen::Vector3f::Zero();
-    Eigen::Vector3f domega_RN_N = Eigen::Vector3f::Zero();
-};
-
 #endif
