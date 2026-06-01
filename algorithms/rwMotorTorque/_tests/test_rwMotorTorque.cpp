b7f7ac72be452b5206a645030bb3abecf66a133c
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-01 14:02:26 -0600

    Use float literals in cpp regression test

--- algorithms/rwMotorTorque/_tests/test_rwMotorTorque.cpp
+++ algorithms/rwMotorTorque/_tests/test_rwMotorTorque.cpp
@@ -2,13 +2,13 @@
 #include <gtest/gtest.h>
 
 TEST(RwMotorTorqueTest, ReferenceTest) {
-    testRwMotorTorque(Eigen::Vector3f{0.1, 0.2, 0.3},
-                      Eigen::Vector3f{0.2, -0.4, 0.7},
+    testRwMotorTorque(Eigen::Vector3f{0.1F, 0.2F, 0.3F},
+                      Eigen::Vector3f{0.2F, -0.4F, 0.7F},
                       std::vector<bool>{false, false, true, false},
                       true,
                       true,
                       4,
-                      std::vector<float>{0.4, 0.1, -0.3, 1.2, 0.4, 0.1, -0.3, 1.2, 0.4, 0.1, -0.3, 1.2},
+                      std::vector<float>{0.4F, 0.1F, -0.3F, 1.2F, 0.4F, 0.1F, -0.3F, 1.2F, 0.4F, 0.1F, -0.3F, 1.2F},
                       3);
 }
 
