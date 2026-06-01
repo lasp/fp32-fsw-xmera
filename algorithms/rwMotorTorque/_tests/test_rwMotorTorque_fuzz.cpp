9e9b08592d8f2f126e8359f8cd6a196b803b2b74
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-01 14:06:43 -0600

    Use RW_MOTOR_TORQUE_MAX_NUM_RW in fuzz test domains

--- algorithms/rwMotorTorque/_tests/test_rwMotorTorque_fuzz.cpp
+++ algorithms/rwMotorTorque/_tests/test_rwMotorTorque_fuzz.cpp
@@ -5,9 +5,9 @@
 FUZZ_TEST(RwMotorTorqueAlgorithmFuzz, testRwMotorTorque)
     .WithDomains(xmera::fuzz::Vector3fInRange(-1e6F, 1e6F),
                  xmera::fuzz::Vector3fInRange(-1e6F, 1e6F),
-                 fuzztest::VectorOf(fuzztest::Arbitrary<bool>()).WithSize(RW_EFF_CNT),
+                 fuzztest::VectorOf(fuzztest::Arbitrary<bool>()).WithSize(RW_MOTOR_TORQUE_MAX_NUM_RW),
                  fuzztest::Arbitrary<bool>(),
                  fuzztest::Arbitrary<bool>(),
-                 fuzztest::InRange(0, RW_EFF_CNT),
-                 fuzztest::VectorOf(fuzztest::InRange(-1.0F, 1.0F)).WithSize(RW_EFF_CNT * 3U),
+                 fuzztest::InRange(0, RW_MOTOR_TORQUE_MAX_NUM_RW),
+                 fuzztest::VectorOf(fuzztest::InRange(-1.0F, 1.0F)).WithSize(RW_MOTOR_TORQUE_MAX_NUM_RW * 3U),
                  fuzztest::InRange(0, 3));
