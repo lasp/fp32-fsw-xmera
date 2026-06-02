8d8a58bf8e9b06df7c8f5170613260a0002b6812
Leah Kiner <leahannkiner@gmail.com>
2026-06-02 14:25:52 -0600

    Remove function for SPE angle calculation

--- algorithms/triad/triadAlgorithm.cpp
+++ algorithms/triad/triadAlgorithm.cpp
@@ -6,27 +6,17 @@
 #include <numbers>
 #include <stdexcept>
 
-static float SPE_angle(const Eigen::Vector3f& v1, const Eigen::Vector3f& v2) {
-    const float dot = v1.dot(v2);
-    const float cross = v1.x() * v2.y() - v1.y() * v2.x();
-
-    float angle = safeAcosf(dot / (v1.norm() * v2.norm()));
-
-    if (cross < 0.0F) {
-        angle = -angle;
-    }
-
-    return angle;
-}
-
-TriadAlgorithm::TriadAlgorithm(const TriadConfig& config) : cfg(config) {}
+TriadAlgorithm::TriadAlgorithm(const TriadConfig& config)
+    : cfg(config) {}
 
 void TriadAlgorithm::setConfig(const TriadConfig& config) { this->cfg = config; }
 
 Eigen::Vector3f TriadAlgorithm::update(const Eigen::Vector3f& rHat_SB_N,
                                        const Eigen::Vector3f& hRefHat_B) const {
     const Eigen::Vector3f hReqHat_N =  this->cfg.getHHat_N();
-    if (const float SPE = SPE_angle(rHat_SB_N, hReqHat_N); fabsf(SPE) < kParallelThresholdRad) {
+
+    const float SPE = safeAcosf(fabsf(rHat_SB_N.dot(hReqHat_N)));
+    if (SPE < kParallelThresholdRad) {
         throw std::runtime_error("sun and earth reference vectors are parallel, Triad can not be used");
     }
 
