4d295c7dd29699002c9a93d7d2c74e8b5af582e9
Leah Kiner <leahannkiner@gmail.com>
2026-06-02 14:23:07 -0600

    Use radians in threshold value

--- algorithms/triad/triadAlgorithm.cpp
+++ algorithms/triad/triadAlgorithm.cpp
@@ -4,18 +4,13 @@
 #include "utilities/fsw/safeMath.h"
 #include <math.h>
 #include <Eigen/Core>
-#include <numbers>
 #include <stdexcept>
-
-static constexpr float kSpeParallelThresholdDeg = 0.5F;
-static constexpr float kRadToDeg = 180.0F / std::numbers::pi_v<float>;
 
 static float SPE_angle(const Eigen::Vector3f& v1, const Eigen::Vector3f& v2) {
     const float dot = v1.dot(v2);
     const float cross = v1.x() * v2.y() - v1.y() * v2.x();
 
     float angle = safeAcosf(dot / (v1.norm() * v2.norm()));
-    angle = angle * kRadToDeg;
 
     if (cross < 0.0F) {
         angle = -angle;
@@ -33,8 +28,8 @@ Eigen::Vector3f TriadAlgorithm::update(const Eigen::Vector3f& sigma_BN,
                                        const Eigen::Vector3f& hRefHat_B) const {
     const Eigen::Matrix3f dcm_BN = mrpToDcm(sigma_BN);
     const Eigen::Vector3f rHat_SB_N = (dcm_BN.transpose() * rHat_SB_B).normalized();
-    const Eigen::Vector3f hReqHat_N = this->cfg.getHHat_N();
-    if (const float SPE = SPE_angle(rHat_SB_N, hReqHat_N); fabsf(SPE) < kSpeParallelThresholdDeg) {
+    const Eigen::Vector3f hReqHat_N =  this->cfg.getHHat_N();
+    if (const float SPE = SPE_angle(rHat_SB_N, hReqHat_N); fabsf(SPE) < kParallelThresholdRad) {
         throw std::runtime_error("sun and earth reference vectors are parallel, Triad can not be used");
     }
 
