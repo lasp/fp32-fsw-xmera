0300e7a649f59d83d03fb68db278109e84f10c63
Leah Kiner <leahannkiner@gmail.com>
2026-06-11 11:28:36 -0600

    Refactor bounded mrp norm property test

--- algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint_fuzz.cpp
+++ algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint_fuzz.cpp
@@ -3,8 +3,6 @@
 
 #include <fuzztest/fuzztest.h>
 
-namespace {
-
 constexpr float kCelestialBodyAlignmentThreshold = 0.017453292519943F;  // [rad] 1 deg
 
 // ---------------------------------------------------------------------------
@@ -34,13 +32,11 @@ FUZZ_TEST(CelestialTwoBodyPointPropertyFuzz, propertyOutputIsFinite)
                  xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // v_BN_N
                  fuzztest::InRange(1e-6F, 1e6F));            // celestialBodyAlignmentThreshold
 
-}  // namespace
-
-FUZZ_TEST(CelestialTwoBodyPointFuzz, fuzzCelestialTwoBodyPointWithSecondaryProperties)
-    .WithDomains(xmera::fuzz::Vector3dInRange(-2e13, 2e13),
-                 xmera::fuzz::Vector3dInRange(-1e4, 1e4),
-                 fuzztest::Filter([](const Eigen::Vector3d& v) { return v.norm() >= 1.0e4; },
-                                  xmera::fuzz::Vector3dInRange(-2e13, 2e13)),
-                 xmera::fuzz::Vector3dInRange(-1e4, 1e4),
-                 xmera::fuzz::Vector3dInRange(-2e13, 2e13),
-                 xmera::fuzz::Vector3dInRange(-1e4, 1e4));
+FUZZ_TEST(CelestialTwoBodyPointPropertyFuzz, propertySigmaNormBounded)
+    .WithDomains(xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // r_PN_N
+                 xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // v_PN_N
+                 xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // r_SN_N
+                 xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // v_SN_N
+                 xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // r_BN_N
+                 xmera::fuzz::Vector3dInRange(-1e6F, 1e6F),  // v_BN_N
+                 fuzztest::InRange(1e-6F, 1e6F));            // celestialBodyAlignmentThreshold
