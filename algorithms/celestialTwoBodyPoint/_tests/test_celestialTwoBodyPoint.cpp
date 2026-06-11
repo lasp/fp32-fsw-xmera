0739a61f84fa98ca19c5662b7a01b398a562604e
Leah Kiner <leahannkiner@gmail.com>
2026-06-11 12:35:37 -0600

    Add CelestialBodiesAlignedUsesFixedConstraintAxis edge case test

--- algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint.cpp
+++ algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint.cpp
@@ -129,3 +129,29 @@ TEST(CelestialTwoBodyPointTest, SpacecraftAtSecondaryReturnsZero) {
         EXPECT_NEAR(result.domega_RN_N[i], 0.0F, tol);
     }
 }
+
+// When the celestial bodies are aligned, the primary body angular momentum vector is used as the
+// secondary constraint axis.
+TEST(CelestialTwoBodyPointTest, CelestialBodiesAlignedUsesFixedConstraintAxis) {
+    const Eigen::Vector3d r_PN_N{-1e4, 0.0, 0.0};
+    const Eigen::Vector3d v_PN_N{0.0, -3e4, 0.0};
+    const Eigen::Vector3d r_SN_N{-1e4, 0.0, 0.0};
+    const Eigen::Vector3d v_SN_N{3e4, 0.0, 0.0};
+    const Eigen::Vector3d r_BN_N{5e2, 5e3, 0.0};
+    const Eigen::Vector3d v_BN_N{-1e4, 1e3, 0.0};
+    const float celestialBodyAlignmentThreshold = 0.017F;
+
+    auto config = CelestialTwoBodyPointConfig::create(celestialBodyAlignmentThreshold);
+    const CelestialTwoBodyPointAlgorithm alg(config);
+
+    CelestialTwoBodyPointOutput result = alg.update(r_PN_N, v_PN_N, r_SN_N, v_SN_N, r_BN_N, v_BN_N);
+    ReferenceCelestialTwoBodyPointOutput expected =
+        referenceCelestialTwoBodyPoint(r_PN_N, v_PN_N, r_SN_N, v_SN_N, r_BN_N, v_BN_N, celestialBodyAlignmentThreshold);
+
+    constexpr float tol = 1e-5F;
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_NEAR(result.sigma_RN[i], static_cast<float>(expected.sigma_RN[i]), tol);
+        EXPECT_NEAR(result.omega_RN_N[i], static_cast<float>(expected.omega_RN_N[i]), tol);
+        EXPECT_NEAR(result.domega_RN_N[i], static_cast<float>(expected.domega_RN_N[i]), tol);
+    }
+}
