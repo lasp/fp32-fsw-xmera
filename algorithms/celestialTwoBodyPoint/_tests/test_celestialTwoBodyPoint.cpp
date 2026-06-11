0f49653f47dc5c0e190ecc6c87b6b0f0ad73ecc2
Leah Kiner <leahannkiner@gmail.com>
2026-06-11 12:34:26 -0600

    Add SpacecraftAtSecondaryReturnsZero edge case test

--- algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint.cpp
+++ algorithms/celestialTwoBodyPoint/_tests/test_celestialTwoBodyPoint.cpp
@@ -105,3 +105,27 @@ TEST(CelestialTwoBodyPointTest, SpacecraftAtPrimaryReturnsZero) {
         EXPECT_NEAR(result.domega_RN_N[i], 0.0F, tol);
     }
 }
+
+// When the relative position between the spacecraft and the secondary celestial body is zero, identity reference
+// attitude and zero reference rates are returned
+TEST(CelestialTwoBodyPointTest, SpacecraftAtSecondaryReturnsZero) {
+    const Eigen::Vector3d r_PN_N{-1e4, 0.0, 0.0};
+    const Eigen::Vector3d v_PN_N{0.0, -3e4, 0.0};
+    const Eigen::Vector3d r_SN_N{1e3, 0.0, 0.0};
+    const Eigen::Vector3d v_SN_N{3e4, 0.0, 0.0};
+    const Eigen::Vector3d r_BN_N{1e3, 0.0, 0.0};
+    const Eigen::Vector3d v_BN_N{-1e4, 1e3, 0.0};
+    const float celestialBodyAlignmentThreshold = 0.017F;
+
+    auto config = CelestialTwoBodyPointConfig::create(celestialBodyAlignmentThreshold);
+    const CelestialTwoBodyPointAlgorithm alg(config);
+
+    CelestialTwoBodyPointOutput result = alg.update(r_PN_N, v_PN_N, r_SN_N, v_SN_N, r_BN_N, v_BN_N);
+
+    constexpr float tol = 1e-6F;
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_NEAR(result.sigma_RN[i], 0.0F, tol);
+        EXPECT_NEAR(result.omega_RN_N[i], 0.0F, tol);
+        EXPECT_NEAR(result.domega_RN_N[i], 0.0F, tol);
+    }
+}
