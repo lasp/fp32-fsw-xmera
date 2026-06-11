0237e79e8e4c1c9a0e610dca7fb811b2bd71e193
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-11 10:12:58 -0600

    Strip license headers from sunlineFilter module

--- algorithms/sunlineFilter/_tests/test_sunlineFilterAlgorithm.cpp
+++ algorithms/sunlineFilter/_tests/test_sunlineFilterAlgorithm.cpp
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: ISC
-// Copyright (c) 2026, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
-
 // Unit tests for SunlineFilterAlgorithm and the SRUKF numerical helpers.
 //
 // Covers:
