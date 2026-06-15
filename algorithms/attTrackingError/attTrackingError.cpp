0adeefa9fef2f92d6923e4fc9ae68c0c60911b25
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-15 13:18:01 -0600

    Add two-phase init lifecycle to AttTrackingError adapter

--- algorithms/attTrackingError/attTrackingError.cpp
+++ algorithms/attTrackingError/attTrackingError.cpp
@@ -1,7 +1,7 @@
 #include "attTrackingError.h"
 
 #include "utilities/fsw/eigenSupport.h"
-#include "utilities/fsw/messageConversionHelpers.hpp"
+#include "utilities/xmera/xmeraLifecycleException.h"
 
 /*! This method performs a complete reset of the module. Local module variables that retain time varying states between
  function calls are reset to their default values.
@@ -16,6 +16,8 @@ void AttTrackingError::reset(uint64_t callTime) {
     if (!this->attNavInMsg.isLinked()) {
         throw std::invalid_argument("attTrackingError.attNavInMsg wasn't connected.");
     }
+    auto config = AttTrackingErrorConfig::create();
+    this->algorithm = std::make_unique<AttTrackingErrorAlgorithm>(config);
 }
 
 /*! The Update method performs reads the Navigation message (containing the spacecraft attitude information), and the
@@ -25,39 +27,29 @@ void AttTrackingError::reset(uint64_t callTime) {
  @param callTime The clock time at which the function was called (nanoseconds)
  */
 void AttTrackingError::updateState(uint64_t callTime) {
-    // Read F32 input messages
-    AttRefMsgF32Payload refF32 = this->attRefInMsg();
+    if (!this->algorithm) {
+        throw XmeraLifecycleException("AttTrackingError reset() has not been called.");
+    }
+
     NavAttMsgF32Payload navF32 = this->attNavInMsg();
+    AttRefMsgF32Payload refF32 = this->attRefInMsg();
 
-    // Convert input C arrays to eigen vectors
-    const Eigen::Vector3f sigma_BN = Eigen::Map<const Eigen::Vector3f>(navF32.sigma_BN);
-    const Eigen::Vector3f omega_BN_B = Eigen::Map<const Eigen::Vector3f>(navF32.omega_BN_B);
-    const Eigen::Vector3f sigma_RN = Eigen::Map<const Eigen::Vector3f>(refF32.sigma_RN);
-    const Eigen::Vector3f omega_RN_N = Eigen::Map<const Eigen::Vector3f>(refF32.omega_RN_N);
-    const Eigen::Vector3f domega_RN_N = Eigen::Map<const Eigen::Vector3f>(refF32.domega_RN_N);
-
-    // Create algorithm input structs
     AttNavInput navIn{};
-    navIn.sigma_BN = sigma_BN;
-    navIn.omega_BN_B = omega_BN_B;
+    navIn.sigma_BN = cArrayToEigenVector(navF32.sigma_BN);
+    navIn.omega_BN_B = cArrayToEigenVector(navF32.omega_BN_B);
 
     AttRefInput refIn{};
-    refIn.sigma_RN = sigma_RN;
-    refIn.omega_RN_N = omega_RN_N;
-    refIn.domega_RN_N = domega_RN_N;
+    refIn.sigma_RN = cArrayToEigenVector(refF32.sigma_RN);
+    refIn.omega_RN_N = cArrayToEigenVector(refF32.omega_RN_N);
+    refIn.domega_RN_N = cArrayToEigenVector(refF32.domega_RN_N);
 
-    // Call the attitude tracking error algorithm
-    const AttGuidOutput guidOut = this->algorithm.update(navIn, refIn);
+    const AttGuidOutput guidOut = this->algorithm->update(navIn, refIn);
 
-    // Create local container for output message
     AttGuidMsgF32Payload attGuidOutF32{};
-
-    // Convert algorithm outputs from eigen vectors to C arrays
     eigenVectorToCArray(guidOut.sigma_BR, attGuidOutF32.sigma_BR);
     eigenVectorToCArray(guidOut.omega_BR_B, attGuidOutF32.omega_BR_B);
     eigenVectorToCArray(guidOut.omega_RN_B, attGuidOutF32.omega_RN_B);
     eigenVectorToCArray(guidOut.domega_RN_B, attGuidOutF32.domega_RN_B);
 
-    // Write the attitude guidance float output message
     this->attGuidOutMsg.write(attGuidOutF32, this->moduleID, callTime);
 }
