bdddd5b4995f860cbefb239b8f1a93922fda368b
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-15 15:08:28 -0600

    Add ConvertStPlatformToBodyConfig validated configuration class

--- algorithms/convertStPlatformToBody/convertStPlatformToBodyAlgorithm.h
+++ algorithms/convertStPlatformToBody/convertStPlatformToBodyAlgorithm.h
@@ -2,8 +2,35 @@
 #define F32XIMERA_CONVERT_ST_PLATFORM_TO_BODY_ALGORITHM_H
 
 #include "convertStPlatformToBodyTypes.h"
+#include "utilities/fsw/freestandingInvalidArgument.h"
+#include "utilities/fsw/validDcmCheck.h"
 
 #include <Eigen/Core>
+
+/*!
+ * @brief Validated configuration for the platform-to-body conversion algorithm.
+ *
+ * An instance can only exist with a valid (orthonormal, det +1) body-to-case mounting DCM.
+ * Construct via ConvertStPlatformToBodyConfig::create(...).
+ */
+class ConvertStPlatformToBodyConfig final {
+   public:
+    static ConvertStPlatformToBodyConfig create(const Eigen::Matrix3f& dcm_CB) {
+        if (!isValidDcmCB(dcm_CB)) {
+            FSW_THROW_INVALID_ARGUMENT("convertStPlatformToBody: dcm_CB must be a valid DCM (orthonormal, det +1)");
+        }
+        return ConvertStPlatformToBodyConfig{dcm_CB};
+    }
+
+    static bool isValidDcmCB(const Eigen::Matrix3f& dcm_CB) { return isValidDcm(dcm_CB); }
+
+    const Eigen::Matrix3f& getDcmCB() const { return dcm_CB; }
+
+   private:
+    explicit ConvertStPlatformToBodyConfig(const Eigen::Matrix3f& dcm_CB) : dcm_CB(dcm_CB) {}
+
+    Eigen::Matrix3f dcm_CB;
+};
 
 class ConvertStPlatformToBodyAlgorithm {
    public:
