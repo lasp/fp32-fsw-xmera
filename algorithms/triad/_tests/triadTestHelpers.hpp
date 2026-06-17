791fbb6fb105de7ace1a71e70a663f518ac75beb
Leah Kiner <leahannkiner@gmail.com>
2026-06-17 15:46:26 -0600

    Update algorithm edge case checks and logic

--- algorithms/triad/_tests/triadTestHelpers.hpp
+++ algorithms/triad/_tests/triadTestHelpers.hpp
@@ -2,6 +2,7 @@
 #define TEST_TRIAD_H
 
 #include "triadAlgorithm.h"
+#include "utilities/fsw/safeMath.h"
 #include <gtest/gtest.h>
 #include <Eigen/Core>
 #include <architecture/utilities/rigidBodyKinematics.hpp>
@@ -14,28 +15,52 @@ inline Eigen::Vector3f referenceTriad(const Eigen::Vector3f& sigma_BN,
                                       const Eigen::Vector3f& a1Hat_B,
                                       const Eigen::Vector3f& hRefHat_B,
                                       const float signOfZHat_N) {
-    const Eigen::Vector3f r2 = hRefHat_B;
-    const Eigen::Vector3f r3 = a1Hat_B.cross(hRefHat_B).normalized();
-    const Eigen::Vector3f r1 = r2.cross(r3);
+    /*! Compute angle between solar array drive axis and thrust direction */
+    const float sadaAxisToThrustAngle = safeAcosf(fabsf(a1Hat_B.dot(hRefHat_B)));
 
-    const Eigen::Vector3f n2 = hReqHat_N;
+    /*! Return current attitude if solar array drive axis and thrust direction are nearly parallel or if either
+     * thrustHat_B or rHat_SB_B are zero */
+    if (sadaAxisToThrustAngle < kParallelThresholdRad || hRefHat_B.stableNorm() == 0.0F ||
+        rHat_SB_B.stableNorm() == 0.0F) {
+        return mrpSwitch(sigma_BN, 1.0F);
+    }
 
-    const Eigen::Matrix3f dcm_BN = mrpToDcm(sigma_BN);
-    const Eigen::Vector3f rHat_SB_N = (dcm_BN.transpose() * rHat_SB_B).normalized();
-    const Eigen::Vector3f n1 = rHat_SB_N.cross(hReqHat_N).normalized();
-    const Eigen::Vector3f n3 = n1.cross(n2);
-
+    /*! Triad (D frame) basis vectors in hub reference frame */
     Eigen::Matrix3f RD;
+    const Eigen::Vector3f r2 = hRefHat_B.normalized();
+    const Eigen::Vector3f r3 = a1Hat_B.cross(hRefHat_B).normalized();
+    const Eigen::Vector3f r1 = r2.cross(r3).normalized();
     RD.col(0) = r1;
     RD.col(1) = r2;
     RD.col(2) = r3;
 
-    Eigen::Matrix3f BD;
-    BD.col(0) = n1;
-    BD.col(1) = n2;
-    BD.col(2) = n3;
+    /*! Compute angle between sun direction and thrust inertial reference direction */
+    const Eigen::Matrix3f dcm_BN = mrpToDcm(sigma_BN);
+    const Eigen::Vector3f rHat_SB_N = (dcm_BN.transpose() * rHat_SB_B).normalized();
+    const float SPE = safeAcosf(fabsf(rHat_SB_N.dot(hReqHat_N)));
 
-    const Eigen::Matrix3f RN = RD * BD.transpose();
+    /*! Triad (D Frame) basis vectors in inertial frame */
+    const Eigen::Vector3f n2 = hReqHat_N;
+    Eigen::Vector3f n1 = Eigen::Vector3f::Zero();
+    Eigen::Vector3f n3 = Eigen::Vector3f::Zero();
+
+    /*! If sun direction and thrust inertial reference are nearly parallel, cross the second triad axis instead with the
+     * configured inertial z-axis */
+    if (fabsf(SPE) < kParallelThresholdRad) {
+        const Eigen::Vector3f zHat_N = (signOfZHat_N * Eigen::Vector3f::UnitZ()).normalized();
+        n3 = zHat_N.cross(n2).normalized();
+        n1 = n2.cross(n3);
+    } else {
+        // Normal triad otherwise
+        n1 = rHat_SB_N.cross(n2).normalized();
+        n3 = n1.cross(n2);
+    }
+    Eigen::Matrix3f ND;
+    ND.col(0) = n1;
+    ND.col(1) = n2;
+    ND.col(2) = n3;
+
+    const Eigen::Matrix3f RN = RD * ND.transpose();
 
     return dcmToMrp(RN);
 }
