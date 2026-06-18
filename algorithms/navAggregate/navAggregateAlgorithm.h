28ecd9cfdc55fa64ded1002a5efbef4ec289a5c7
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-18 16:24:00 -0600

    Add NavAggregateConfig validated configuration class

--- algorithms/navAggregate/navAggregateAlgorithm.h
+++ algorithms/navAggregate/navAggregateAlgorithm.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+#include "utilities/fsw/freestandingInvalidArgument.h"
 #include <Eigen/Core>
 #include <array>
 
@@ -30,6 +31,70 @@ struct InputNavTransData {
 struct AggregateOutput {
     InputNavAttData navAttOut;     /*!< attitude navigation output */
     InputNavTransData navTransOut; /*!< translation navigation output */
+};
+
+/*! Message-selection indices for the attitude navigation aggregation. */
+struct NavAggregateAttSelection {
+    uint32_t attTimeIdx{};   //!< [-] index of the message providing the attitude message time
+    uint32_t attIdx{};       //!< [-] index of the message providing the inertial MRP
+    uint32_t rateIdx{};      //!< [-] index of the message providing the attitude rate
+    uint32_t sunIdx{};       //!< [-] index of the message providing the sun-pointing vector
+    uint32_t attMsgCount{};  //!< [-] number of attitude messages available as inputs
+};
+
+/*! Message-selection indices for the translational navigation aggregation. */
+struct NavAggregateTransSelection {
+    uint32_t transTimeIdx{};   //!< [-] index of the message providing the translation message time
+    uint32_t posIdx{};         //!< [-] index of the message providing the inertial position
+    uint32_t velIdx{};         //!< [-] index of the message providing the inertial velocity
+    uint32_t dvIdx{};          //!< [-] index of the message providing the accumulated DV
+    uint32_t transMsgCount{};  //!< [-] number of translation messages available as inputs
+};
+
+/*!
+ * @brief Validated configuration for the navigation aggregation algorithm.
+ *
+ * An instance can only exist when every selection index is less than MAX_AGG_NAV_MSG and each message count
+ * does not exceed MAX_AGG_NAV_MSG. Construct via NavAggregateConfig::create(...).
+ */
+class NavAggregateConfig final {
+   public:
+    static NavAggregateConfig create(const NavAggregateAttSelection& attSelection,
+                                     const NavAggregateTransSelection& transSelection) {
+        if (!isValidAttSelection(attSelection)) {
+            FSW_THROW_INVALID_ARGUMENT(
+                "navAggregate: attitude selection indices must be < MAX_AGG_NAV_MSG and attMsgCount must be "
+                "<= MAX_AGG_NAV_MSG.");
+        }
+        if (!isValidTransSelection(transSelection)) {
+            FSW_THROW_INVALID_ARGUMENT(
+                "navAggregate: translation selection indices must be < MAX_AGG_NAV_MSG and transMsgCount must be "
+                "<= MAX_AGG_NAV_MSG.");
+        }
+        return {attSelection, transSelection};
+    }
+
+    static bool isValidAttSelection(const NavAggregateAttSelection& attSelection) {
+        return attSelection.attTimeIdx < MAX_AGG_NAV_MSG && attSelection.attIdx < MAX_AGG_NAV_MSG &&
+               attSelection.rateIdx < MAX_AGG_NAV_MSG && attSelection.sunIdx < MAX_AGG_NAV_MSG &&
+               attSelection.attMsgCount <= MAX_AGG_NAV_MSG;
+    }
+
+    static bool isValidTransSelection(const NavAggregateTransSelection& transSelection) {
+        return transSelection.transTimeIdx < MAX_AGG_NAV_MSG && transSelection.posIdx < MAX_AGG_NAV_MSG &&
+               transSelection.velIdx < MAX_AGG_NAV_MSG && transSelection.dvIdx < MAX_AGG_NAV_MSG &&
+               transSelection.transMsgCount <= MAX_AGG_NAV_MSG;
+    }
+
+    const NavAggregateAttSelection& getAttSelection() const { return this->attSelection; }
+    const NavAggregateTransSelection& getTransSelection() const { return this->transSelection; }
+
+   private:
+    NavAggregateConfig(const NavAggregateAttSelection& attSelection, const NavAggregateTransSelection& transSelection)
+        : attSelection(attSelection), transSelection(transSelection) {}
+
+    NavAggregateAttSelection attSelection;
+    NavAggregateTransSelection transSelection;
 };
 
 class NavAggregateAlgorithm {
