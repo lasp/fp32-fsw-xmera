bf26f56e64bc2ab0faf228b400377cab9536b467
Julian Hammerl <julian.hammerl@hotmail.com>
2026-06-23 12:25:16 -0600

    Define ThrustPulsingRegime in the algorithm header

--- algorithms/thrFiringSchmitt/thrFiringSchmittTypes.h
+++ algorithms/thrFiringSchmitt/thrFiringSchmittTypes.h
@@ -1,17 +1,4 @@
 #ifndef F32XMERA_THR_FIRING_SCHMITT_TYPES_H
 #define F32XMERA_THR_FIRING_SCHMITT_TYPES_H
 
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*! @brief Thrust pulsing regime. */
-typedef enum ThrustPulsingRegime { ON_PULSING = 0, OFF_PULSING = 1 } ThrustPulsingRegime;
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
-
 #endif  // F32XMERA_THR_FIRING_SCHMITT_TYPES_H
