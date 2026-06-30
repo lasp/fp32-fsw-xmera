1d11308dced7a7b3743c01a6a7b3aa721e13725e
Chun-Wei Kong <j2855001@gmail.com>
2026-06-30 04:10:09 -0600

    Add FlybyPointConfig validated configuration class

--- algorithms/flybyPoint/flybyPointAlgorithm.h
+++ algorithms/flybyPoint/flybyPointAlgorithm.h
@@ -1,6 +1,7 @@
 #ifndef F32XMERA_FLYBY_POINT_ALGORITHM_H
 #define F32XMERA_FLYBY_POINT_ALGORITHM_H
 
+#include "utilities/fsw/freestandingInvalidArgument.h"
 #include <Eigen/Dense>
 
 /*! @brief Structure containing the attitude guidance output of the algorithm */
@@ -13,6 +14,76 @@ struct AttGuideOutput {
     bool maxAccelerationTrigger = false;  // true if the predicted acceleration exceeds the maximum acceleration of the
                                           // spacecraft
     bool positionKnowledgeExceedTrigger = false;  // true if the position error exceeds a-priori sigma bound
+};
+
+class FlybyPointConfig final {
+   public:
+    static FlybyPointConfig create(double timeBetweenFilterData,
+                                   float toleranceForCollinearity,
+                                   int signOfOrbitNormalFrameVector,
+                                   float maximumRateThreshold,
+                                   float maximumAccelerationThreshold,
+                                   float positionKnowledgeSigma) {
+        if (!isValidTimeBetweenFilterData(timeBetweenFilterData)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: timeBetweenFilterData must be > 0");
+        }
+        if (!isValidToleranceForCollinearity(toleranceForCollinearity)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: toleranceForCollinearity must be > 0");
+        }
+        if (!isValidSignOfOrbitNormalFrameVector(signOfOrbitNormalFrameVector)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: signOfOrbitNormalFrameVector must be +1 or -1");
+        }
+        if (!isValidMaximumRateThreshold(maximumRateThreshold)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: maximumRateThreshold must be > 0");
+        }
+        if (!isValidMaximumAccelerationThreshold(maximumAccelerationThreshold)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: maximumAccelerationThreshold must be > 0");
+        }
+        if (!isValidPositionKnowledgeSigma(positionKnowledgeSigma)) {
+            FSW_THROW_INVALID_ARGUMENT("flybyPoint: positionKnowledgeSigma must be > 0");
+        }
+        return {timeBetweenFilterData,
+                toleranceForCollinearity,
+                signOfOrbitNormalFrameVector,
+                maximumRateThreshold,
+                maximumAccelerationThreshold,
+                positionKnowledgeSigma};
+    }
+
+    static bool isValidTimeBetweenFilterData(double t) { return t > 0.0; }
+    static bool isValidToleranceForCollinearity(float t) { return t > 0.0F; }
+    static bool isValidSignOfOrbitNormalFrameVector(int s) { return s == 1 || s == -1; }
+    static bool isValidMaximumRateThreshold(float r) { return r > 0.0F; }
+    static bool isValidMaximumAccelerationThreshold(float a) { return a > 0.0F; }
+    static bool isValidPositionKnowledgeSigma(float s) { return s > 0.0F; }
+
+    double getTimeBetweenFilterData() const { return timeBetweenFilterData; }
+    float getToleranceForCollinearity() const { return toleranceForCollinearity; }
+    int getSignOfOrbitNormalFrameVector() const { return signOfOrbitNormalFrameVector; }
+    float getMaximumRateThreshold() const { return maximumRateThreshold; }
+    float getMaximumAccelerationThreshold() const { return maximumAccelerationThreshold; }
+    float getPositionKnowledgeSigma() const { return positionKnowledgeSigma; }
+
+   private:
+    FlybyPointConfig(double timeBetweenFilterData,
+                     float toleranceForCollinearity,
+                     int signOfOrbitNormalFrameVector,
+                     float maximumRateThreshold,
+                     float maximumAccelerationThreshold,
+                     float positionKnowledgeSigma)
+        : timeBetweenFilterData(timeBetweenFilterData),
+          toleranceForCollinearity(toleranceForCollinearity),
+          signOfOrbitNormalFrameVector(signOfOrbitNormalFrameVector),
+          maximumRateThreshold(maximumRateThreshold),
+          maximumAccelerationThreshold(maximumAccelerationThreshold),
+          positionKnowledgeSigma(positionKnowledgeSigma) {}
+
+    double timeBetweenFilterData;
+    float toleranceForCollinearity;
+    int signOfOrbitNormalFrameVector;
+    float maximumRateThreshold;
+    float maximumAccelerationThreshold;
+    float positionKnowledgeSigma;
 };
 
 /*! @brief A class to perform flyby pointing */
