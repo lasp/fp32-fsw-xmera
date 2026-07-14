0302060f46c1c1683d7ecb2cf4a04746d591f4d6
Julian Hammerl <julian.hammerl@hotmail.com>
2026-07-14 11:39:12 -0600

    Refactor pytest

--- algorithms/thrusterPlatformReference/_UnitTest/test_thrusterPlatformReference.py
+++ algorithms/thrusterPlatformReference/_UnitTest/test_thrusterPlatformReference.py
@@ -1,11 +1,6 @@
 import pytest
-import os, inspect, random
+import random
 import numpy as np
-
-filename = inspect.getframeinfo(inspect.currentframe()).filename
-path = os.path.dirname(os.path.abspath(filename))
-bskName = 'xmera'
-splitPath = path.split(bskName)
 
 
 # Import all the modules that are going to be called in this simulation
@@ -21,13 +16,12 @@ from xmera.architecture import sim_model
 # of the multiple test runs for this test.  Note that the order in that you add the parametrize method
 # matters for the documentation in that it impacts the order in which the test arguments are shown.
 # The first parametrize arguments are shown last in the pytest argument list
-@pytest.mark.parametrize("seed", list(np.linspace(1,10,10)))
-@pytest.mark.parametrize("delta_CM", [0.1, 0.2, 0.3])
-@pytest.mark.parametrize("K", [0,1,5,10])
-@pytest.mark.parametrize("thetaMax", [-1, np.pi/36])
+@pytest.mark.parametrize("seed", list(np.linspace(1, 10, 10)))
+@pytest.mark.parametrize("delta_cm", [0.1, 0.2, 0.3])
+@pytest.mark.parametrize("k", [0, 1, 5, 10])
+@pytest.mark.parametrize("theta_max", [-1, np.pi / 36])
 @pytest.mark.parametrize("accuracy", [1e-4])
-# update "module" in this function name to reflect the module name
-def test_platformRotation(show_plots, delta_CM, K, thetaMax, seed, accuracy):
+def test_thruster_platform_reference(show_plots, delta_cm, k, theta_max, seed, accuracy):
     r"""
     **Validation Test Description**
 
@@ -43,8 +37,8 @@ def test_platformRotation(show_plots, delta_CM, K, thetaMax, seed, accuracy):
     of test parameters.
 
     Args:
-        delta_CM (m): magnitude of the center of mass shift, whose direction is generated randomly
-        K (Hz): proportional gain of the momentum dumping control law
+        delta_cm (m): magnitude of the center of mass shift, whose direction is generated randomly
+        k (Hz): proportional gain of the momentum dumping control law
         seed (-): seed is varied to randomly change the shift in the center of mass
         accuracy (float): accuracy within which results are considered to match the truth values.
 
@@ -65,11 +59,10 @@ def test_platformRotation(show_plots, delta_CM, K, thetaMax, seed, accuracy):
     The offset vectors provided as input parameters ensure that a solution exists, such that the Unit Test can correctly
     assess the alignment of the thruster. This is, in general, not guaranteed.
     """
-    # each test method requires a single assert method to be called
-    platformRotationTestFunction(show_plots, delta_CM, K, thetaMax, seed, accuracy)
+    thruster_platform_reference_test_function(show_plots, delta_cm, k, theta_max, seed, accuracy)
 
 
-def platformRotationTestFunction(show_plots, delta_CM, K, thetaMax, seed, accuracy):
+def thruster_platform_reference_test_function(show_plots, delta_cm, k, theta_max, seed, accuracy):
 
     random.seed(seed)
 
@@ -78,94 +71,94 @@ def platformRotationTestFunction(show_plots, delta_CM, K, thetaMax, seed, accura
     r_BM_M = np.array([0.0, 0.1, 1.4])
     r_FM_F = np.array([0.0, 0.0, -0.1])
     r_TF_F = np.array([-0.01, 0.03, 0.02])
-    T_F    = np.array([1.0, 1.0, 10.0])
+    T_F = np.array([1.0, 1.0, 10.0])
 
-    r_CB_B = np.array([0,0,0]) + np.random.rand(3)
-    r_CB_B = r_CB_B / np.linalg.norm(r_CB_B) * delta_CM
+    r_CB_B = np.array([0, 0, 0]) + np.random.rand(3)
+    r_CB_B = r_CB_B / np.linalg.norm(r_CB_B) * delta_cm
 
-    unitTaskName = "unitTask"                # arbitrary name (don't change)
-    unitProcessName = "TestProcess"          # arbitrary name (don't change)
+    unit_task_name = "unitTask"          # arbitrary name (don't change)
+    unit_process_name = "TestProcess"    # arbitrary name (don't change)
     sim_model.setDefaultLogLevel(sim_model.BSK_WARNING)
 
     # Create a sim module as an empty container
-    unitTestSim = SimulationBaseClass.SimBaseClass()
+    unit_test_sim = SimulationBaseClass.SimBaseClass()
 
     # Create test thread
-    testProcessRate = macros.sec2nano(1)     # update process rate update time
-    testProc = unitTestSim.CreateNewProcess(unitProcessName)
-    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+    test_process_rate = macros.sec2nano(1)     # update process rate update time
+    test_proc = unit_test_sim.CreateNewProcess(unit_process_name)
+    test_proc.addTask(unit_test_sim.CreateNewTask(unit_task_name, test_process_rate))
 
     # Construct algorithm and associated C++ container
     platform = thrusterPlatformReferenceF32.ThrusterPlatformReference()
     platform.modelTag = "platformReference"
 
     # Add test module to runtime call list
-    unitTestSim.AddModelToTask(unitTaskName, platform)
+    unit_test_sim.AddModelToTask(unit_task_name, platform)
 
     # Initialize the test module configuration data
     platform.sigma_MB = sigma_MB
     platform.r_BM_M = r_BM_M
     platform.r_FM_F = r_FM_F
-    platform.K = K
+    platform.K = k
     platform.Ki = 0
-    platform.theta1Max = thetaMax
-    platform.theta2Max = thetaMax
+    platform.theta1Max = theta_max
+    platform.theta2Max = theta_max
 
     # Create input vehicle configuration msg
-    inputVehConfigMsgData = messaging.VehicleConfigMsgF32Payload()
-    inputVehConfigMsgData.CoM_B = r_CB_B
-    inputVehConfigMsg = messaging.VehicleConfigMsgF32().write(inputVehConfigMsgData)
-    platform.vehConfigInMsg.subscribeTo(inputVehConfigMsg)
+    input_veh_config_msg_data = messaging.VehicleConfigMsgF32Payload()
+    input_veh_config_msg_data.CoM_B = r_CB_B
+    input_veh_config_msg = messaging.VehicleConfigMsgF32().write(input_veh_config_msg_data)
+    platform.vehConfigInMsg.subscribeTo(input_veh_config_msg)
 
     # Create input THR Config Msg
-    THRConfig = messaging.THRConfigMsgF32Payload()
-    THRConfig.rThrust_B = r_TF_F
-    THRConfig.maxThrust = np.linalg.norm(T_F)
-    THRConfig.tHatThrust_B = T_F / THRConfig.maxThrust
-    thrConfigFMsg = messaging.THRConfigMsgF32().write(THRConfig)
-    platform.thrusterConfigFInMsg.subscribeTo(thrConfigFMsg)
+    thr_config = messaging.THRConfigMsgF32Payload()
+    thr_config.rThrust_B = r_TF_F
+    thr_config.maxThrust = np.linalg.norm(T_F)
+    thr_config.tHatThrust_B = T_F / thr_config.maxThrust
+    thr_config_f_msg = messaging.THRConfigMsgF32().write(thr_config)
+    platform.thrusterConfigFInMsg.subscribeTo(thr_config_f_msg)
 
     # Create input RW configuration msg
-    inputRWConfigMsgData = messaging.RWArrayConfigMsgF32Payload()
-    inputRWConfigMsgData.GsMatrix_B = [1,0,0,0,1,0,0,0,1]
-    inputRWConfigMsgData.JsList = [0.01, 0.01, 0.01]
-    inputRWConfigMsgData.numRW = 3
-    inputRWConfigMsgData.uMax = [0.001, 0.001, 0.001]
-    inputRWConfigMsg = messaging.RWArrayConfigMsgF32().write(inputRWConfigMsgData)
-    platform.rwConfigDataInMsg.subscribeTo(inputRWConfigMsg)
+    input_rw_config_msg_data = messaging.RWArrayConfigMsgF32Payload()
+    input_rw_config_msg_data.GsMatrix_B = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+    input_rw_config_msg_data.JsList = [0.01, 0.01, 0.01]
+    input_rw_config_msg_data.numRW = 3
+    input_rw_config_msg_data.uMax = [0.001, 0.001, 0.001]
+    input_rw_config_msg = messaging.RWArrayConfigMsgF32().write(input_rw_config_msg_data)
+    platform.rwConfigDataInMsg.subscribeTo(input_rw_config_msg)
 
     # Create input RW speeds msg
-    inputRWSpeedsMsgData = messaging.RWSpeedMsgF32Payload()
-    inputRWSpeedsMsgData.wheelSpeeds = [100, 100, 100]
-    inputRWSpeedsMsg = messaging.RWSpeedMsgF32().write(inputRWSpeedsMsgData)
-    platform.rwSpeedsInMsg.subscribeTo(inputRWSpeedsMsg)
+    input_rw_speeds_msg_data = messaging.RWSpeedMsgF32Payload()
+    input_rw_speeds_msg_data.wheelSpeeds = [100, 100, 100]
+    input_rw_speeds_msg = messaging.RWSpeedMsgF32().write(input_rw_speeds_msg_data)
+    platform.rwSpeedsInMsg.subscribeTo(input_rw_speeds_msg)
 
     # Setup logging on the test module output messages so that we get all the writes to it
-    ref1Log = platform.hingedRigidBodyRef1OutMsg.recorder()
-    unitTestSim.AddModelToTask(unitTaskName, ref1Log)
-    ref2Log = platform.hingedRigidBodyRef2OutMsg.recorder()
-    unitTestSim.AddModelToTask(unitTaskName, ref2Log)
-    bodyHeadingLog = platform.bodyHeadingOutMsg.recorder()
-    unitTestSim.AddModelToTask(unitTaskName, bodyHeadingLog)
-    thrusterTorqueLog = platform.thrusterTorqueOutMsg.recorder()
-    unitTestSim.AddModelToTask(unitTaskName, thrusterTorqueLog)
-    thrConfigBLog = platform.thrusterConfigBOutMsg.recorder()
-    unitTestSim.AddModelToTask(unitTaskName, thrConfigBLog)
+    ref1_log = platform.hingedRigidBodyRef1OutMsg.recorder()
+    unit_test_sim.AddModelToTask(unit_task_name, ref1_log)
+    ref2_log = platform.hingedRigidBodyRef2OutMsg.recorder()
+    unit_test_sim.AddModelToTask(unit_task_name, ref2_log)
+    body_heading_log = platform.bodyHeadingOutMsg.recorder()
+    unit_test_sim.AddModelToTask(unit_task_name, body_heading_log)
+    thruster_torque_log = platform.thrusterTorqueOutMsg.recorder()
+    unit_test_sim.AddModelToTask(unit_task_name, thruster_torque_log)
+    thr_config_b_log = platform.thrusterConfigBOutMsg.recorder()
+    unit_test_sim.AddModelToTask(unit_task_name, thr_config_b_log)
 
     # Need to call the self-init and cross-init methods
-    unitTestSim.InitializeSimulation()
+    unit_test_sim.InitializeSimulation()
 
     # Set the simulation time.
     # NOTE: the total simulation time may be longer than this value. The
     # simulation is stopped at the next logging event on or after the
     # simulation end time.
-    unitTestSim.ConfigureStopTime(macros.sec2nano(0.5))        # seconds to stop simulation
+    unit_test_sim.ConfigureStopTime(macros.sec2nano(0.5))        # seconds to stop simulation
 
     # Begin the simulation time run set above
-    unitTestSim.ExecuteSimulation()
+    unit_test_sim.ExecuteSimulation()
 
-    theta1 = ref1Log.theta[0]
-    theta2 = ref2Log.theta[0]
+    theta1 = ref1_log.theta[0]
+    theta2 = ref2_log.theta[0]
 
     FM = rbk.euler1232C([theta1, theta2, 0.0])
     MB = rbk.MRP2C(sigma_MB)
@@ -175,40 +168,40 @@ def platformRotationTestFunction(show_plots, delta_CM, K, thetaMax, seed, accura
     r_CM_F = np.matmul(FM, r_CM_M)
     r_CT_F = r_CM_F - r_FM_F - r_TF_F
 
-    offset = np.linalg.norm(np.cross(r_CT_F,T_F) / np.linalg.norm(np.array(r_CT_F)) / np.linalg.norm(np.array(T_F)))
+    offset = np.linalg.norm(np.cross(r_CT_F, T_F) / np.linalg.norm(np.array(r_CT_F)) / np.linalg.norm(np.array(T_F)))
 
-    # check if the CM offset is zero if control gain K is also 0
-    if K == 0 and thetaMax < 0:
-        np.testing.assert_allclose(offset, 0.0, rtol=0, atol=accuracy, verbose=True)
+    # check if the CM offset is zero if control gain k is also 0
+    if k == 0 and theta_max < 0:
+        np.testing.assert_allclose(offset, 0.0, rtol=accuracy, atol=accuracy, verbose=True)
 
-    T_B_hat_sim = bodyHeadingLog.rHat_XB_B[0]               # simulation result
+    T_B_hat_sim = body_heading_log.rHat_XB_B[0]             # simulation result
     FB = np.matmul(FM, MB)
     T_B = np.matmul(FB.transpose(), T_F)
     T_B_hat = T_B / np.linalg.norm(T_B)                     # truth value
 
     # compare the module results to the python computation for body-frame thruster direction
-    np.testing.assert_allclose(T_B_hat_sim, T_B_hat, rtol=0, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(T_B_hat_sim, T_B_hat, rtol=accuracy, atol=accuracy, verbose=True)
 
-    L_B_sim = thrusterTorqueLog.torqueRequestBody[0]        # simulation result
+    L_B_sim = thruster_torque_log.torqueRequestBody[0]     # simulation result
     L_F = np.cross(r_CT_F, T_F)
-    L_B = np.matmul(FB.transpose(),L_F)
+    L_B = np.matmul(FB.transpose(), L_F)
 
     # compare the module results to the python computation for body-frame cmd torque
-    np.testing.assert_allclose(L_B_sim, L_B, rtol=0, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(L_B_sim, L_B, rtol=accuracy, atol=accuracy, verbose=True)
 
     # compare the module results to the python computation for thruster configuration in B frame
     r_TB_B = r_CB_B - np.matmul(FB.transpose(), r_CT_F)
-    r_TB_B_sim = thrConfigBLog.rThrust_B[0]
-    tHat_B_sim = thrConfigBLog.tHatThrust_B[0]
-    tMax_sim = thrConfigBLog.maxThrust[0]
-    np.testing.assert_allclose(r_TB_B_sim, r_TB_B, rtol=0, atol=accuracy, verbose=True)
-    np.testing.assert_allclose(tHat_B_sim, T_B_hat, rtol=0, atol=accuracy, verbose=True)
-    np.testing.assert_allclose(tMax_sim, np.linalg.norm(T_B), rtol=0, atol=accuracy, verbose=True)
+    r_TB_B_sim = thr_config_b_log.rThrust_B[0]
+    tHat_B_sim = thr_config_b_log.tHatThrust_B[0]
+    tMax_sim = thr_config_b_log.maxThrust[0]
+    np.testing.assert_allclose(r_TB_B_sim, r_TB_B, rtol=accuracy, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(tHat_B_sim, T_B_hat, rtol=accuracy, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(tMax_sim, np.linalg.norm(T_B), rtol=accuracy, atol=accuracy, verbose=True)
 
     # compare the output reference angle
-    if thetaMax > 0:
-        np.testing.assert_array_less(theta1, thetaMax + accuracy, verbose=True)
-        np.testing.assert_array_less(theta2, thetaMax + accuracy, verbose=True)
+    if theta_max > 0:
+        np.testing.assert_array_less(theta1, theta_max + accuracy, verbose=True)
+        np.testing.assert_array_less(theta2, theta_max + accuracy, verbose=True)
 
     return
 
@@ -218,11 +211,11 @@ def platformRotationTestFunction(show_plots, delta_CM, K, thetaMax, seed, accura
 # stand-along python script
 #
 if __name__ == "__main__":
-    test_platformRotation(
-                 False,                   # show_plots
-                 0.1,                     # delta_CM
-                 0,                       # K
-                 -1,                      # thetaMax
-                 np.random.rand(1)[0],    # seed
-                 1e-4                     # accuracy
-               )
+    test_thruster_platform_reference(
+        False,                   # show_plots
+        0.1,                     # delta_cm
+        0,                       # k
+        -1,                      # theta_max
+        np.random.rand(1)[0],    # seed
+        1e-4                     # accuracy
+    )
