7bc4909afd344e5341d93f238ababd8f01d2c01d
Julian Hammerl <julian.hammerl@hotmail.com>
2026-07-22 15:22:49 -0600

    Initialize class members

--- algorithms/dvExecuteGuidance/dvExecuteGuidance.h
+++ algorithms/dvExecuteGuidance/dvExecuteGuidance.h
@@ -20,14 +20,14 @@ class DvExecuteGuidance : public SysModel {
     ReadFunctor<DvBurnCmdMsgPayload> burnDataInMsg;    /*!< [-] commanded burn input message */
     Message<THRArrayOnTimeCmdMsgPayload> thrCmdOutMsg; /*!< [-] thruster command on time output message */
     Message<DvExecutionDataMsgPayload> burnExecOutMsg; /*!< [-] burn execution output message */
-    double dvInit[3];                                  /*!< [m/s] DV reading off the accelerometers at burn start*/
-    uint32_t burnExecuting;                            /*!< [-] Flag indicating whether the burn is in progress or not*/
-    uint32_t burnComplete;                             /*!< [-] Flag indicating that burn has completed successfully*/
-    double burnTime;                                   /*!< [s] Burn time to be used for telemetry*/
-    uint64_t prevCallTime;                             /*!< [-] Call time register for computing total burn time*/
-    double minTime;                                    /*!< [s] Minimum count of burn time allowed to elapse*/
-    double maxTime;                                    /*!< [s] Maximum count of burn time allowed to elapse*/
-    double defaultControlPeriod;                       /*!< [s] Default control period used for first call*/
+    double dvInit[3]{};                                /*!< [m/s] DV reading off the accelerometers at burn start*/
+    uint32_t burnExecuting{};                          /*!< [-] Flag indicating whether the burn is in progress or not*/
+    uint32_t burnComplete{};                           /*!< [-] Flag indicating that burn has completed successfully*/
+    double burnTime{};                                 /*!< [s] Burn time to be used for telemetry*/
+    uint64_t prevCallTime{};                           /*!< [-] Call time register for computing total burn time*/
+    double minTime{};                                  /*!< [s] Minimum count of burn time allowed to elapse*/
+    double maxTime{};                                  /*!< [s] Maximum count of burn time allowed to elapse*/
+    double defaultControlPeriod{};                     /*!< [s] Default control period used for first call*/
 };
 
 #endif
