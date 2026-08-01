0e932bd8c4c0b5dc71244896740931a231769e8f
Leah Kiner <leahannkiner@gmail.com>
2026-08-01 12:24:49 -0600

    Clarify valid angle check

--- algorithms/gimbalAxisToMotorAngles/gimbalAxisToMotorAnglesAlgorithm.cpp
+++ algorithms/gimbalAxisToMotorAngles/gimbalAxisToMotorAnglesAlgorithm.cpp
@@ -179,9 +179,8 @@ MotorAngles GimbalAxisToMotorAnglesAlgorithm::bilinearlyInterpolateMotorAngles(c
     float motor2Angle{};
     bool validInterpolation{};
     // Interpolate the motor angles if all bounding angles are valid
-    if (motor1AngleLLBound >= 0.0F && motor1AngleLUBound >= 0.0F && motor1AngleULBound >= 0.0F &&
-        motor1AngleUUBound >= 0.0F && motor2AngleLLBound >= 0.0F && motor2AngleLUBound >= 0.0F &&
-        motor2AngleULBound >= 0.0F && motor2AngleUUBound >= 0.0F) {
+    if (motorLLBounds.isValidInterpolation && motorLUBounds.isValidInterpolation &&
+        motorULBounds.isValidInterpolation && motorUUBounds.isValidInterpolation) {
         motor1Angle = static_cast<float>(bilinearInterpolation(gimbalAngle1LBound,
                                                                gimbalAngle1UBound,
                                                                gimbalAngle2LBound,
@@ -256,8 +255,7 @@ MotorAngles GimbalAxisToMotorAnglesAlgorithm::linearlyInterpolateMotorAngles(con
     float motor2Angle{};
     bool validInterpolation{};
     // Linearly interpolate if the pulled angles are valid
-    if (motor1AngleLBound >= 0.0F && motor1AngleUBound >= 0.0F && motor2AngleLBound >= 0.0F &&
-        motor2AngleUBound >= 0.0F) {
+    if (lowerMotorBounds.isValidInterpolation && upperMotorBounds.isValidInterpolation) {
         motor1Angle = static_cast<float>(linearInterpolation(
             gimbalAngleLBound, gimbalAngleUBound, motor1AngleLBound, motor1AngleUBound, boundedAngle));
         motor2Angle = static_cast<float>(linearInterpolation(
