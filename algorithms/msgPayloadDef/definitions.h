db64563f05ff25464e91fbc161b8bc0c2f39b368
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-08-03 20:54:12 -0600

    Source the shared counts from the mission parameters header

--- algorithms/msgPayloadDef/definitions.h
+++ algorithms/msgPayloadDef/definitions.h
@@ -1,15 +1,22 @@
 #ifndef MSG_DEFINITIONS_H
 #define MSG_DEFINITIONS_H
 
-#include <cstdint>
+#include <mission/parameters.h>
 
-#define MAX_NUM_CSS_SENSORS 32
-inline constexpr std::uint32_t kMaxNumCssSensors = MAX_NUM_CSS_SENSORS;
-#define MAX_EFF_CNT 36
-inline constexpr std::uint32_t kMaxThrusterCount = MAX_EFF_CNT;
-inline constexpr std::uint32_t kMimuCount = 3U;
-#define RW_EFF_CNT 36
+// FP32 algorithm specific constants
 #define MAX_MEASUREMENT_NUMBER 5
 #define MAX_MEASUREMENT_VECTOR 5
+
+// The C boundary types size their arrays from the macros above so that they stay
+// parseable as C. The typed constants below are for C++ callers only.
+#ifdef __cplusplus
+
+#include <cstdint>
+
+inline constexpr std::uint32_t kMaxNumCssSensors = MAX_NUM_CSS_SENSORS;
+inline constexpr std::uint32_t kMaxThrusterCount = MAX_EFF_CNT;
+inline constexpr std::uint32_t kMimuCount = 3U;
+
+#endif  // __cplusplus
 
 #endif  // MSG_DEFINITIONS_H
