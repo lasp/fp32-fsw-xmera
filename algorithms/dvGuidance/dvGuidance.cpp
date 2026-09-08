a26fa940174ad356ea42cf3b58af6a311064fb2a
ShamsaSabekZaei <albshamsa@gmail.com>
2026-09-08 16:29:50 +0400

    Rename variables for clarity

--- algorithms/dvGuidance/dvGuidance.cpp
+++ algorithms/dvGuidance/dvGuidance.cpp
@@ -17,18 +17,17 @@ void DvGuidance::updateState(const uint64_t callTime) {
         throw XmeraLifecycleException("DvGuidance reset() has not been called.");
     }
 
-    const DvBurnCmdMsgF32Payload localBurnData = this->burnDataInMsg();
+    const DvBurnCmdMsgF32Payload burnData = this->burnDataInMsg();
+    const Eigen::Vector3f dvInrtlCmd = cArrayToEigenVector3<float>(burnData.dvInrtlCmd);
+    const Eigen::Vector3f dvRotVecUnit = cArrayToEigenVector3<float>(burnData.dvRotVecUnit);
 
-    const Eigen::Vector3f dvInrtlCmd = cArrayToEigenVector3<float>(localBurnData.dvInrtlCmd);
-    const Eigen::Vector3f dvRotVecUnit = cArrayToEigenVector3<float>(localBurnData.dvRotVecUnit);
+    const DvGuidanceOutput out =
+        this->algorithm->update(dvInrtlCmd, dvRotVecUnit, burnData.dvRotVecMag, burnData.burnStartTime, callTime);
 
-    const DvGuidanceOutput out = this->algorithm->update(
-        dvInrtlCmd, dvRotVecUnit, localBurnData.dvRotVecMag, localBurnData.burnStartTime, callTime);
+    AttRefMsgF32Payload attRefOut = AttRefMsgF32Payload();
+    eigenVectorToCArray(out.sigma_RN, attRefOut.sigma_RN);
+    eigenVectorToCArray(out.omega_RN_N, attRefOut.omega_RN_N);
+    eigenVectorToCArray(out.domega_RN_N, attRefOut.domega_RN_N);
 
-    AttRefMsgF32Payload attCmd = {};
-    eigenVectorToCArray(out.sigma_RN, attCmd.sigma_RN);
-    eigenVectorToCArray(out.omega_RN_N, attCmd.omega_RN_N);
-    eigenVectorToCArray(out.domega_RN_N, attCmd.domega_RN_N);
-
-    this->attRefOutMsg.write(attCmd, this->moduleID, callTime);
+    this->attRefOutMsg.write(attRefOut, this->moduleID, callTime);
 }
