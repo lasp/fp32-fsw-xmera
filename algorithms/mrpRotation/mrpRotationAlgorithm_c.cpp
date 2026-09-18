b3cb78b806817a5e840a131db146203986b17eae
Julian Hammerl <julian.hammerl@hotmail.com>
2026-09-17 18:16:57 -0600

    Flatten the mrpRotation C shim configuration arguments

--- algorithms/mrpRotation/mrpRotationAlgorithm_c.cpp
+++ algorithms/mrpRotation/mrpRotationAlgorithm_c.cpp
@@ -7,10 +7,11 @@
 #include <Eigen/Core>
 
 namespace {
-MrpRotationConfig configFromC(const MrpRotationConfig_c& c) {
-    return MrpRotationConfig::create(cArrayToEigenVector3<float>(c.initialSigmaRR0.data),
-                                     cArrayToEigenVector3<float>(c.omegaRR0R.data),
-                                     c.controlPeriod);
+MrpRotationConfig configFromC(const Vector3f_c& initialSigmaRR0,
+                              const Vector3f_c& omegaRR0R,
+                              const float controlPeriod) {
+    return MrpRotationConfig::create(
+        cArrayToEigenVector3<float>(initialSigmaRR0.data), cArrayToEigenVector3<float>(omegaRR0R.data), controlPeriod);
 }
 
 MrpRotationAttRefInputs attRefFromC(const MrpRotationAttRefInputs_c& c) {
@@ -30,14 +31,20 @@ MrpRotationOutput_c outputToC(const MrpRotationOutput& out) {
 }
 }  // namespace
 
-MrpRotationAlgorithmHandle* MrpRotationAlgorithm_create(const MrpRotationConfig_c* config) {
-    return fsw::createHandle<::MrpRotationAlgorithm, MrpRotationAlgorithmHandle>(configFromC(*config));
+MrpRotationAlgorithmHandle* MrpRotationAlgorithm_create(const Vector3f_c* initialSigmaRR0,
+                                                        const Vector3f_c* omegaRR0R,
+                                                        const float controlPeriod) {
+    return fsw::createHandle<::MrpRotationAlgorithm, MrpRotationAlgorithmHandle>(
+        configFromC(*initialSigmaRR0, *omegaRR0R, controlPeriod));
 }
 
 void MrpRotationAlgorithm_destroy(MrpRotationAlgorithmHandle* self) { fsw::deleteHandle<::MrpRotationAlgorithm>(self); }
 
-void MrpRotationAlgorithm_setConfig(MrpRotationAlgorithmHandle* self, const MrpRotationConfig_c* config) {
-    fsw::fromHandle<::MrpRotationAlgorithm>(self)->setConfig(configFromC(*config));
+void MrpRotationAlgorithm_setConfig(MrpRotationAlgorithmHandle* self,
+                                    const Vector3f_c* initialSigmaRR0,
+                                    const Vector3f_c* omegaRR0R,
+                                    const float controlPeriod) {
+    fsw::fromHandle<::MrpRotationAlgorithm>(self)->setConfig(configFromC(*initialSigmaRR0, *omegaRR0R, controlPeriod));
 }
 
 MrpRotationOutput_c MrpRotationAlgorithm_update(MrpRotationAlgorithmHandle* self,
